190b802e4a2a0534af521118cba840c66f1b0ab2
nicholas.blumhardt <unknown@kiln.example.com>
2007-12-17 10:41:30 +0000

    Added the Module base type.

---
+++ src/Autofac.Tests/Builder/ModuleFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Autofac.Builder;
+
+namespace Autofac.Tests.Builder
+{
+    [TestFixture]
+    public class ModuleFixture
+    {
+        class ObjectModule : Module
+        {
+            protected override void Load()
+            {
+                this.Register(new object());
+            }
+        }
+
+        [Test]
+        public void LoadsRegistrations()
+        {
+            var container = new Container();
+            new ObjectModule().Configure(container);
+            Assert.IsTrue(container.IsRegistered<object>());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DetectsNullContainer()
+        {
+            new ObjectModule().Configure(null);
+        }
+    }
+}
