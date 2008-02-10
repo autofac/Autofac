457cfd348124800e47c93e00ed778e80e043b962
nicholas.blumhardt <unknown@kiln.example.com>
2008-02-10 03:18:17 +0000

    Better implementation of tagged contexts (Issue 28).

---
+++ src/Test/Autofac.Tests.Extras.TaggedContexts/ContainerBuilderExtensionsFixture.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Autofac.Builder;
+using Autofac.Extras.TaggedContexts;
+
+namespace Autofac.Tests.Extras.TaggedContexts
+{
+    [TestFixture]
+    public class ContainerBuilderExtensionsFixture
+    {
+        enum Tag { None, Outer, Middle, Inner }
+
+        [Test]
+        public void OuterSatisfiesInnerResolutions()
+        {
+            var builder = new ContainerBuilder();
+            builder.EnableTaggedContexts(Tag.Outer, Tag.None);
+
+            int instantiations = 0;
+
+            builder.RegisterInContext(c => { instantiations++; return ""; }, Tag.Outer);
+
+            var outer = builder.Build();
+            var middle = outer.CreateTaggedInnerContainer(Tag.Middle);
+            var inner = middle.CreateTaggedInnerContainer(Tag.Inner);
+
+            middle.Resolve<string>();
+            outer.Resolve<string>();
+            inner.Resolve<string>();
+
+            Assert.AreEqual(1, instantiations);
+        }
+
+        [Test]
+        [Ignore("Does not support anonymous inner containers.")]
+        public void AnonymousInnerContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.EnableTaggedContexts(Tag.Outer, Tag.None);
+
+            int instantiations = 0;
+
+            builder.RegisterInContext(c => { instantiations++; return ""; }, Tag.Outer);
+
+            var outer = builder.Build();
+            var anon = outer.CreateInnerContainer();
+
+            anon.Resolve<string>();
+            outer.Resolve<string>();
+
+            Assert.AreEqual(1, instantiations);
+        }
+
+        [Test]
+        [ExpectedException(typeof(DependencyResolutionException))]
+        public void InnerRegistrationNotAccessibleToOuter()
+        {
+            var builder = new ContainerBuilder();
+            builder.EnableTaggedContexts(Tag.Outer, Tag.None);
+            builder.RegisterInContext(c => "", Tag.Middle);
+            
+            var outer = builder.Build();
+
+            Assert.IsTrue(outer.IsRegistered<string>());
+            outer.Resolve<string>();
+        }
+
+        [Test]
+        public void TaggedRegistrationsAccessibleThroughNames()
+        {
+            var name = "Name";
+
+            var builder = new ContainerBuilder();
+            builder.EnableTaggedContexts(Tag.Outer, Tag.None);
+            builder.RegisterInContext(c => "", Tag.Outer).Named(name);
+
+            var outer = builder.Build();
+
+            var s = (string)outer.Resolve(new NamedService(name));
+            Assert.IsNotNull(s);
+        }
+    }
+}
