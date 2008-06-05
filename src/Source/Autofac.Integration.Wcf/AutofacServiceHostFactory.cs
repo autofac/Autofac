9b0f59be0f6b1f4a0a745ca75e4ba674e49a74f7
nicholas.blumhardt <unknown@kiln.example.com>
2008-06-05 08:35:06 +0000

    Allowed named services in WCF configuration.

--- src/Source/Autofac.Integration.Wcf/AutofacServiceHostFactory.cs
+++ src/Source/Autofac.Integration.Wcf/AutofacServiceHostFactory.cs
@@ -26,6 +26,7 @@
 using System;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
+using System.Globalization;
 
 namespace Autofac.Integration.Wcf
 {
@@ -38,6 +39,44 @@ namespace Autofac.Integration.Wcf
         /// The container from which service instances will be retrieved.
         /// </summary>
         public static IContainer Container { get; set; }
+
+        /// <summary>
+        /// Creates a <see cref="T:System.ServiceModel.ServiceHost"/> with specific base addresses and initializes it with specified data.
+        /// </summary>
+        /// <param name="constructorString">The initialization data passed to the <see cref="T:System.ServiceModel.ServiceHostBase"/> instance being constructed by the factory.</param>
+        /// <param name="baseAddresses">The <see cref="T:System.Array"/> of type <see cref="T:System.Uri"/> that contains the base addresses for the service hosted.</param>
+        /// <returns>
+        /// A <see cref="T:System.ServiceModel.ServiceHost"/> with specific base addresses.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// 	<paramref name="baseAddress"/> is null.</exception>
+        /// <exception cref="T:System.InvalidOperationException">There is no hosting context provided or <paramref name="constructorString"/> is null or empty.</exception>
+        public override ServiceHostBase CreateServiceHost(string constructorString, Uri[] baseAddresses)
+        {
+            if (constructorString == null)
+                throw new ArgumentNullException("constructorString");
+
+            if (constructorString == String.Empty)
+                throw new ArgumentOutOfRangeException("constructorString");
+
+            IComponentRegistration registration = null;
+            if (!Container.TryGetDefaultRegistrationFor(new NamedService(constructorString), out registration))
+            {
+                Type serviceType = Type.GetType(constructorString, false);
+                if (serviceType != null)
+                    Container.TryGetDefaultRegistrationFor(new TypedService(serviceType), out registration);
+            }
+
+            if (registration == null)
+                throw new InvalidOperationException(
+                    string.Format(CultureInfo.CurrentCulture, AutofacServiceHostFactoryResources.ServiceNotRegistered, constructorString));
+
+            if (!registration.Descriptor.BestKnownImplementationType.IsClass)
+                throw new InvalidOperationException(
+                    string.Format(CultureInfo.CurrentCulture, AutofacServiceHostFactoryResources.ImplementationTypeUnknown, constructorString, registration));
+
+            return CreateServiceHost(registration.Descriptor.BestKnownImplementationType, baseAddresses);
+        }
 
         /// <summary>
         /// Creates a <see cref="T:System.ServiceModel.ServiceHost"/> for a specified type of service with a specific base address.
@@ -54,6 +93,9 @@ namespace Autofac.Integration.Wcf
 
             if (baseAddresses == null)
                 throw new ArgumentNullException("baseAddresses");
+
+            if (Container == null)
+                throw new InvalidOperationException(AutofacServiceHostFactoryResources.ContainerIsNull);
 
 			return new AutofacServiceHost(Container, serviceType, baseAddresses);
 		}
