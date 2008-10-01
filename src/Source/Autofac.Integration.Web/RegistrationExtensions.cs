5b40de399f7207f434d8f3c584c82a21661d0e15
nicholas.blumhardt <unknown@kiln.example.com>
2008-10-01 04:31:01 +0000

    Added HttpRequestScoped() extension method to make API more discoverable.

---
+++ src/Source/Autofac.Integration.Web/RegistrationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac.Registrars;
+
+namespace Autofac.Integration.Web
+{
+    /// <summary>
+    /// Extends registrars to support an HttpContextScoped() member.
+    /// </summary>
+    public static class RegistrationExtensions
+    {
+        /// <summary>
+        /// Share one instance of the component within the context of a single
+        /// HTTP request (synonym for <see cref="IRegistrar{T}.ContainerScoped()"/>).
+        /// </summary>
+        /// <typeparam name="TSyntax">Builder syntax type.</typeparam>
+        /// <param name="registrar">The registrar being used to create the registration.</param>
+        /// <returns>The registrar, allowing registration to continue.</returns>
+        public static TSyntax HttpRequestScoped<TSyntax>(this TSyntax registrar)
+            where TSyntax : class, IRegistrar<TSyntax>
+        {
+            if (registrar == null) throw new ArgumentNullException("registrar");
+
+            return registrar.ContainerScoped();
+        }
+    }
+}
---
+++ src/Test/Autofac.Tests.Integration.Web/RegistrationExtensionsFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Autofac.Builder;
+using Autofac.Component.Scope;
+using Autofac.Component;
+using Autofac.Integration.Web;
+
+namespace Autofac.Tests.Integration.Web
+{
+    [TestFixture]
+    public class RegistrationExtensionsFixture
+    {
+        [Test]
+        public void HttpRequestScopedIsASynonymForContainerScoped()
+        {
+            var cb = new ContainerBuilder();
+            var regId = cb.Register<object>().HttpRequestScoped().Id;
+            var c = cb.Build();
+            var reg = (Registration)c.ComponentRegistrations
+                .Where(cr => cr.Descriptor.Id == regId)
+                .Single();
+
+            Assert.IsInstanceOfType(typeof(ContainerScope), reg.Scope);
+        }
+    }
+}
