52372e641526a2b4066700bec5069b38dae0e77c
nicholas.blumhardt <unknown@kiln.example.com>
2009-02-09 00:16:56 +0000

    Added test to verify Issue 92 (pass parameters to service list activation.)

---
+++ src/Test/Autofac.Tests/Component/Activation/ServiceListActivatorFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Autofac.Builder;
+
+namespace Autofac.Tests.Component.Activation
+{
+    [TestFixture]
+    public class ServiceListActivatorFixture
+    {
+        [Test]
+        public void ResolveParametersPassedThroughToCollectionElements()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterCollection<string>();
+            builder.Register((c, p) => p.Named<string>("s"))
+                .FactoryScoped()
+                .MemberOf<IEnumerable<string>>();
+            var container = builder.Build();
+
+            var s = "Howdy, Earth!";
+            var strings = container.Resolve<IEnumerable<string>>(new NamedParameter("s", s));
+            Assert.IsTrue(strings.Contains(s));
+        }
+    }
+}
