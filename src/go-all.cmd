4e23c167c327613e3f8a176633b099edfa6be458
rinat.abdullin <unknown@kiln.example.com>
2009-03-07 11:26:16 +0000

    Removed net20 from go-all.cmd

--- src/go-all.cmd
+++ src/go-all.cmd
@@ -1,4 +1,3 @@
 @echo off
 @Resource\Nant\Nant.exe -buildfile:Autofac.build %*
 @Resource\Nant\Nant.exe -buildfile:Autofac.build silverlight %*
-@Resource\Nant\Nant.exe -buildfile:Autofac.build net20 %*
