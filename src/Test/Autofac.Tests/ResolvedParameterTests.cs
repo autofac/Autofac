f12bbef78915788106d6c6486ea8a9e33a87fe4d
nicholas.blumhardt <unknown@kiln.example.com>
2009-08-02 00:40:37 +0000

    Added test case demonstrating use of ResolvedParameter to construct decorator chains from open generic types. (Issue 98.)

--- src/Test/Autofac.Tests/ResolvedParameterTests.cs
+++ src/Test/Autofac.Tests/ResolvedParameterTests.cs
@@ -24,5 +24,43 @@ namespace Autofac.Tests
             var s = c.Resolve<string>();
             Assert.AreEqual("aaaaa", s);
         }
+
+        interface ISomething<T> { }
+
+        class ConcreteSomething<T> : ISomething<T> { }
+
+        class SomethingDecorator<T> : ISomething<T>
+        {
+            public ISomething<T> Decorated { get; private set; }
+
+            public SomethingDecorator(ISomething<T> decorated)
+            {
+                Decorated = decorated;
+            }
+        }
+
+        [Test]
+        public void CanConstructDecoratorChainFromOpenGenericTypes()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterGeneric(typeof(ConcreteSomething<>));
+
+            var decoratedSomethingArgument = new ResolvedParameter(
+                (pi, c) => pi.Name == "decorated",
+                (pi, c) => c.Resolve(typeof(ConcreteSomething<>).MakeGenericType(
+                                pi.ParameterType.GetGenericArguments())));
+
+            builder.RegisterGeneric(typeof(SomethingDecorator<>))
+                .As(typeof(ISomething<>))
+                .WithArguments(decoratedSomethingArgument);
+
+            var container = builder.Build();
+
+            var concrete = container.Resolve<ISomething<int>>();
+
+            Assert.IsInstanceOfType(typeof(SomethingDecorator<int>), concrete);
+            Assert.IsInstanceOfType(typeof(ConcreteSomething<int>), ((SomethingDecorator<int>)concrete).Decorated);
+        }
     }
 }
