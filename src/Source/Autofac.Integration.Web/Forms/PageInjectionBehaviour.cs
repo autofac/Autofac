b88b39c0a6e4bd7418f7b6d3e1d475096bfbaa75
nicholas.blumhardt <unknown@kiln.example.com>
2010-04-12 11:14:56 +0000

    Fixed formatting of license messages.

--- src/Source/Autofac.Integration.Web/Forms/PageInjectionBehaviour.cs
+++ src/Source/Autofac.Integration.Web/Forms/PageInjectionBehaviour.cs
@@ -1,49 +1,27 @@
-﻿// Contributed by Nicholas Blumhardt 2008-01-28
-
+﻿// Contributed by // This software is part of the Autofac IoC container
 // Copyright (c) 2010 Autofac Contributors
-
+// http://autofac.org
 //
-
 // Permission is hereby granted, free of charge, to any person
-
 // obtaining a copy of this software and associated documentation
-
 // files (the "Software"), to deal in the Software without
-
 // restriction, including without limitation the rights to use,
-
 // copy, modify, merge, publish, distribute, sublicense, and/or sell
-
 // copies of the Software, and to permit persons to whom the
-
 // Software is furnished to do so, subject to the following
-
 // conditions:
-
 //
-
 // The above copyright notice and this permission notice shall be
-
 // included in all copies or substantial portions of the Software.
-
 //
-
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-
 // OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-
 // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-
 // HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-
 // OTHER DEALINGS IN THE SOFTWARE.
-
 
 using System;
 using System.Web.UI;
@@ -68,7 +46,7 @@ namespace Autofac.Integration.Web.Forms
             if (target == null)
                 throw new ArgumentNullException("target");
 
-            Func<object, object> injector = GetInjector(context);
+            var injector = GetInjector(context);
 
             DoInjection(injector, target);
         }
@@ -100,7 +78,7 @@ namespace Autofac.Integration.Web.Forms
                 page.PreLoad += (s, e) => InjectUserControls(injector, page);
         }
 
-        void InjectUserControls(Func<object, object> injector, Control parent)
+        static void InjectUserControls(Func<object, object> injector, Control parent)
         {
             if (injector == null)
                 throw new ArgumentNullException("injector");
@@ -108,15 +86,15 @@ namespace Autofac.Integration.Web.Forms
             if (parent == null)
                 throw new ArgumentNullException("parent");
 
-            if (parent.Controls != null)
+            if (parent.Controls == null) 
+                return;
+
+            foreach (Control control in parent.Controls)
             {
-                foreach (Control control in parent.Controls)
-                {
-                    var uc = control as UserControl;
-                    if (uc != null)
-                        injector(uc);
-                    InjectUserControls(injector, control);
-                }
+                var uc = control as UserControl;
+                if (uc != null)
+                    injector(uc);
+                InjectUserControls(injector, control);
             }
         }
     }
