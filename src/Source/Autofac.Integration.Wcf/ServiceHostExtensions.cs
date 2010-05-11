92d234934daa6bedc2903fed974f56317c1b19cc
alex.meyergleaves@gmail.com <unknown@kiln.example.com>
2010-05-11 15:43:22 +0000

    WCF Integration: Added an extension method for ServiceHostBase that makes configuring the Service Behavior easier when self-hosting.

---
+++ src/Source/Autofac.Integration.Wcf/ServiceHostExtensions.cs
@@ -0,0 +1,73 @@
+﻿// This software is part of the Autofac IoC container
+// Copyright (c) 2010 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.ServiceModel;
+using Autofac.Core;
+
+namespace Autofac.Integration.Wcf
+{
+	/// <summary>
+	///		Adds dependency injection related methods to service hosts.
+	/// </summary>
+	public static class ServiceHostExtensions
+	{
+		/// <summary>
+		/// Adds the custom service behavior required for dependency injection.
+		/// </summary>
+		/// <typeparam name="T">The web service contract type.</typeparam>
+		/// <param name="serviceHost">The service host.</param>
+		/// <param name="container">The container.</param>
+		public static void AddDependencyInjectionBehavior<T>(this ServiceHostBase serviceHost, IContainer container)
+		{
+			if (container == null) throw new ArgumentNullException("container");
+
+			AddDependencyInjectionBehavior(serviceHost, typeof(T), container);
+		}
+
+		/// <summary>
+		/// Adds the custom service behavior required for dependency injection.
+		/// </summary>
+		/// <param name="serviceHost">The service host.</param>
+		/// <param name="contractType">The web service contract type.</param>
+		/// <param name="container">The container.</param>
+		public static void AddDependencyInjectionBehavior(this ServiceHostBase serviceHost, Type contractType, IContainer container)
+		{
+			if (contractType == null) throw new ArgumentNullException("contractType");
+			if (container == null) throw new ArgumentNullException("container");
+
+			IComponentRegistration registration;
+			if (!container.ComponentRegistry.TryGetRegistration(new TypedService(contractType), out registration))
+			{
+				string message = string.Format(ServiceHostExtensionsResources.ContractTypeNotRegistered, contractType.FullName);
+				throw new ArgumentException(message, "contractType");
+			}
+
+			AutofacDependencyInjectionServiceBehavior behavior = new AutofacDependencyInjectionServiceBehavior(
+				container, serviceHost.Description.ServiceType, registration);
+			serviceHost.Description.Behaviors.Add(behavior);
+		}
+	}
+}
