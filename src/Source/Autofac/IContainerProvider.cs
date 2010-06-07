70e63a0a164807dc5efbc5d9486cea913f7c8ec5
travis.illig <unknown@kiln.example.com>
2010-06-07 16:26:57 +0000

    Moved IContainerProvider to Autofac main assembly and updated namespace to be "Autofac." Required so WCF can use the container provider model.

--- src/Source/Autofac/IContainerProvider.cs
+++ src/Source/Autofac/IContainerProvider.cs
@@ -22,7 +22,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-namespace Autofac.Integration.Web
+namespace Autofac
 {
     /// <summary>
     /// Provides global and per-request Autofac containers in an
