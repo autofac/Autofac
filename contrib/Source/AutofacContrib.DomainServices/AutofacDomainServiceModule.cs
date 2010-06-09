3c14d849ffaea21dcfe3dec39187cec9ccb878e3
carlhoerberg <unknown@kiln.example.com>
2010-06-09 15:45:09 +0000

    Added license. Made AutofacDomainServiceModule only Attaching OnActivating event to all DomainService registrations and not try to registrate anything by it self.

--- contrib/Source/AutofacContrib.DomainServices/AutofacDomainServiceModule.cs
+++ contrib/Source/AutofacContrib.DomainServices/AutofacDomainServiceModule.cs
@@ -1,39 +1,49 @@
-using System.Reflection;
-using System.ServiceModel.DomainServices.Server;
+// This software is part of the Autofac IoC container
+// Copyright (c) 2007 - 2010 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 using Autofac;
+using Autofac.Core;
+using System.ServiceModel.DomainServices.Server;
 
 namespace AutofacContrib.DomainServices
 {
-    public class AutofacDomainServiceModule : Autofac.Module
+    /// <summary>
+    /// Initializes all registrated DomainServices. 
+    /// </summary>
+    public class AutofacDomainServiceModule : Module
     {
-        private readonly Assembly[] assemblies;
-
-        /// <summary>
-        /// Registers all classes deriving from DomainService in the current assembly.
-        /// </summary>
-        public AutofacDomainServiceModule()
+        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry, IComponentRegistration registration)
         {
-            assemblies = new[] { Assembly.GetCallingAssembly() };
-        }
-
-        /// <summary>
-        /// Registers all classes deriving from DomainService in the specified assemblies.
-        /// </summary>
-        public AutofacDomainServiceModule(params Assembly[] assemblies)
-        {
-            this.assemblies = assemblies;
-        }
-
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterAssemblyTypes(assemblies).AssignableTo<DomainService>()
-                .OnActivating(args =>
-                {
-                    var ds = (DomainService)args.Instance;
-                    var context = args.Parameters.TypedAs<DomainServiceContext>();
-                    if (context != null)
-                        ds.Initialize(context);
-                });
+            registration.Activating += (sender, args) =>
+            {
+                var ds = args.Instance as DomainService;
+                if (ds == null) return;
+                var context = args.Parameters.TypedAs<DomainServiceContext>();
+                if (context != null)
+                    ds.Initialize(context);
+            };
         }
     }
 }
