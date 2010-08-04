418c112b14978715bc7b366884e7adb24b965c05
nicholas.blumhardt <unknown@kiln.example.com>
2010-08-04 13:38:57 +0000

    Added .UseWcfSafeRelease() for closing client channels without concealing earlier exceptions. Needs the eyes of an expert...

--- src/Source/Autofac.Integration.Wcf/AutofacDependencyInjectionServiceBehavior.cs
+++ src/Source/Autofac.Integration.Wcf/AutofacDependencyInjectionServiceBehavior.cs
@@ -37,9 +37,9 @@ namespace Autofac.Integration.Wcf
     /// <summary>
     /// Sets the instance provider to an AutofacInstanceProvider.
     /// </summary>
-	public class AutofacDependencyInjectionServiceBehavior : IServiceBehavior
-	{
-		private readonly IContainer _container;
+    public class AutofacDependencyInjectionServiceBehavior : IServiceBehavior
+    {
+        private readonly IContainer _container;
         private readonly Type _implementationType;
         private readonly IComponentRegistration _registration;
 
@@ -49,8 +49,8 @@ namespace Autofac.Integration.Wcf
         /// <param name="container">The container.</param>
         /// <param name="implementationType"></param>
         /// <param name="registration"></param>
-		public AutofacDependencyInjectionServiceBehavior(IContainer container, Type implementationType, IComponentRegistration registration)
-		{
+        public AutofacDependencyInjectionServiceBehavior(IContainer container, Type implementationType, IComponentRegistration registration)
+        {
             if (container == null)
                 throw new ArgumentNullException("container");
 
@@ -63,18 +63,18 @@ namespace Autofac.Integration.Wcf
             _container = container;
             _implementationType = implementationType;
             _registration = registration;
-		}
+        }
 
-		#region IServiceBehavior Members
+        #region IServiceBehavior Members
 
         /// <summary>
         /// Provides the ability to inspect the service host and the service description to confirm that the service can run successfully.
         /// </summary>
         /// <param name="serviceDescription">The service description.</param>
         /// <param name="serviceHostBase">The service host that is currently being constructed.</param>
-		public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-		{
-		}
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+        }
 
         /// <summary>
         /// Provides the ability to pass custom data to binding elements to support the contract implementation.
@@ -83,19 +83,19 @@ namespace Autofac.Integration.Wcf
         /// <param name="serviceHostBase">The host of the service.</param>
         /// <param name="endpoints">The service endpoints.</param>
         /// <param name="bindingParameters">Custom objects to which binding elements have access.</param>
-		public void AddBindingParameters(
-			ServiceDescription serviceDescription, ServiceHostBase serviceHostBase,
-			Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
-		{
-		}
+        public void AddBindingParameters(
+            ServiceDescription serviceDescription, ServiceHostBase serviceHostBase,
+            Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+        {
+        }
 
         /// <summary>
         /// Provides the ability to change run-time property values or insert custom extension objects such as error handlers, message or parameter interceptors, security extensions, and other custom extension objects.
         /// </summary>
         /// <param name="serviceDescription">The service description.</param>
         /// <param name="serviceHostBase">The host that is currently being built.</param>
-		public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-		{
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
             if (serviceDescription == null)
                 throw new ArgumentNullException("serviceDescription");
 
@@ -121,8 +121,8 @@ namespace Autofac.Integration.Wcf
                     }
                 }
             }
-		}
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
---
+++ src/Source/Autofac.Integration.Wcf/RegistrationExtensions.cs
@@ -0,0 +1,76 @@
+﻿// This software is part of the Autofac IoC container
+// Copyright (c) 2010 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.ServiceModel;
+using Autofac.Builder;
+
+namespace Autofac.Integration.Wcf
+{
+    /// <summary>
+    /// Extend the registration syntax with WCF-specific helpers.
+    /// </summary>
+    public static class RegistrationExtensions
+    {
+        /// <summary>
+        /// Dispose the channel instance in such a way that exceptions 
+        /// </summary>
+        /// <typeparam name="TLimit">Registration limit type.</typeparam>
+        /// <typeparam name="TActivatorData">Activator data type.</typeparam>
+        /// <typeparam name="TRegistrationStyle">Registration style.</typeparam>
+        /// <param name="registration">Registration to set release action for.</param>
+        /// <returns>Registration builder allowing the registration to be configured.</returns>
+        /// <remarks>This will eat exceptions generated in the closing of the channel.</remarks>
+        public static IRegistrationBuilder<TLimit, TActivatorData, TRegistrationStyle>
+            UseWcfSafeRelease<TLimit, TActivatorData, TRegistrationStyle>(
+                this IRegistrationBuilder<TLimit, TActivatorData, TRegistrationStyle> registration)
+        {
+            if (registration == null) throw new ArgumentNullException("registration");
+            return registration.OnRelease(CloseChannel);
+        }
+
+        static void CloseChannel<T>(T channel)
+        {
+            var disp = (IClientChannel) channel;
+            try
+            {
+                disp.Close();
+            }
+            catch (TimeoutException)
+            {
+                disp.Abort();
+            }
+            catch (CommunicationException)
+            {
+                disp.Abort();
+            }
+            catch (Exception)
+            {
+                disp.Abort();
+                throw;
+            }
+        }
+    }
+}
