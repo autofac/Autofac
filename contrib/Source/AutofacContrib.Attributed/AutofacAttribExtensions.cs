4f98d21492cab3c30259f3ad83dcd85697cb180c
sdhebert <unknown@kiln.example.com>
2010-08-08 11:46:52 +0500

    added the initial attribute extension to the project

---
+++ contrib/Source/AutofacContrib.Attributed/AutofacAttribExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using AutofacContrib.Attributed.MEF;
+
+namespace AutofacContrib.Attributed
+{
+    public static class AutofacAttribExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <typeparam name="TMetadata"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="assemblies"></param>
+        public static void RegisterUsingMetadataAttributes<TInterface, TMetadata>(this ContainerBuilder builder, params Assembly[] assemblies)
+        {
+            var aggregateCatalog = new AggregateCatalog(from a in assemblies select new AssemblyCatalog(a));
+
+            var container = new ExportInfoCompositionContainer(aggregateCatalog);
+
+            var exports = container.GetExportsWithTargetType<TInterface, TMetadata>();
+
+
+            foreach (var export in exports)
+            {
+                // first, register the target type in the container
+                builder.RegisterType(export.InstantiationType);
+
+                // next, create a Lazy<t,meta> registration wired up to the strongly typed metadata
+                // make sure we hold a local variable for proper closure behavior
+                var item = export;
+                builder.Register(
+                    c => new Lazy<TInterface, TMetadata>(() => (TInterface)c.Resolve(item.InstantiationType),
+                                                         item.Value.Metadata));
+
+            }
+        }
+
+    }
+}
