618887f371c7e80bd5eb2db72a42fa78d997780d
sdhebert <unknown@kiln.example.com>
2010-08-12 14:41:13 +0500

    dropped the mef attribution requirements on scenario 1

--- contrib/Source/AutofacContrib.Attributed/AutofacAttributeExtensions.cs
+++ contrib/Source/AutofacContrib.Attributed/AutofacAttributeExtensions.cs
@@ -8,19 +8,26 @@ using Autofac.Features.Metadata;
 
 namespace AutofacContrib.Attributed
 {
-    public static class AutofacAttribExtensions
+    public static class AutofacAttributeExtensions
     {
-        public static void RegisterUsingMetadataAttributes<TInterface, TMetadata>(this ContainerBuilder builder, params Assembly[] assemblies)
-        {
-            builder.RegisterUsingMetadataAttributes<TInterface, TMetadata>(p => true, assemblies);
-        }
+        #region discovery suppport methods
 
-
+        /// <summary>
+        /// a shortcircuit method of looking at attributes to see if they are, in turn, attributed with a Metadata attribute
+        /// </summary>
+        /// <param name="typeInfo">type under investigation</param>
+        /// <returns>true if any metadata attributed attributes are found</returns>
         private static bool HasMetadataAttribute(Type typeInfo)
         {
             return typeInfo.GetCustomAttributes(true).Cast<Attribute>().Any(info => info.GetType().GetCustomAttributes(typeof(MetadataAttributeAttribute), false).Count() > 0);
         }
 
+
+        /// <summary>
+        /// retrieves a dictionary of public properties on an attribute
+        /// </summary>
+        /// <param name="attribute">attribute being queried</param>
+        /// <returns>dictionary of property names and their values</returns>
         private static IDictionary<string,object> GetProperties(Attribute attribute)
         {
             return attribute.GetType().GetProperties().Where(propertyInfo => propertyInfo.CanRead &&
@@ -32,10 +39,25 @@ namespace AutofacContrib.Attributed
 
         }
 
+        /// <summary>
+        /// retrieves the strongly typed metadata instances associated with a given target type
+        /// </summary>
+        /// <typeparam name="TMetadata">metadata type</typeparam>
+        /// <param name="targetType">instance being interrogated for metadata</param>
+        /// <returns>enumerable set of metadata associated with the target type</returns>
         private static IEnumerable<TMetadata> GetStronglyTypedMetadata<TMetadata>(Type targetType)
         {
             return from Attribute attribute in targetType.GetCustomAttributes(true) where attribute.GetType().GetCustomAttributes(typeof(MetadataAttributeAttribute), false).Count() >0 select AttributedModelServices.GetMetadataView<TMetadata>(GetProperties(attribute));
         }
+
+
+        #endregion
+
+        public static void RegisterUsingMetadataAttributes<TInterface, TMetadata>(this ContainerBuilder builder, params Assembly[] assemblies)
+        {
+            builder.RegisterUsingMetadataAttributes<TInterface, TMetadata>(p => true, assemblies);
+        }
+
 
         public static void RegisterUsingMetadataAttributes<TInterface, TMetadata>(this ContainerBuilder builder, Predicate<TMetadata> inclusionPredicate, params Assembly[] assemblies)
         {
@@ -45,10 +67,10 @@ namespace AutofacContrib.Attributed
                                                                       where type.IsClass && type.GetInterface(typeof (TInterface).Name) != null && HasMetadataAttribute(type)
                                                                       select type)).SelectMany(targetTypes => targetTypes))
             {
+                // register the target instance
                 builder.RegisterType(targetType);
 
-                // get all TMetadata instances
-
+                // get and register all TMetadata instances servicing the IEnumerable<Lazy<..>> and IEnumerable<Meta<..>> resolutions
                 foreach (var metadata in GetStronglyTypedMetadata<TMetadata>(targetType).Where(a => inclusionPredicate(a)))
                 {
                     // hold onto the ambient properties in local scope declarations to service the lambda-lifted closure
--- contrib/Test/AutofacContrib.Tests.Attributed/ScenarioTypes/ExportScenario1DiscoveryTargets.cs
+++ contrib/Test/AutofacContrib.Tests.Attributed/ScenarioTypes/ExportScenario1DiscoveryTargets.cs
@@ -1,8 +1,6 @@
-﻿using System.ComponentModel.Composition;
-
+﻿
 namespace AutofacContrib.Tests.Attributed.ScenarioTypes
 {
     [ExportScenario1Metadata("Hello")]
-    [Export(typeof(IExportScenario1))]
     public class ExportScenario1 : IExportScenario1 { }
 }
