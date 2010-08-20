dd7db3eadfd2b5b7e68b6fb82e0390f44f4f84ae
sdhebert <unknown@kiln.example.com>
2010-08-20 08:47:39 +0500

    general cleanup

--- contrib/Test/AutofacContrib.Tests.Attributed/ScenarioTypes/WeakTypedMetadataAttributeScenario.cs
+++ contrib/Test/AutofacContrib.Tests.Attributed/ScenarioTypes/WeakTypedMetadataAttributeScenario.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
 
 namespace AutofacContrib.Tests.Attributed.ScenarioTypes
 {
