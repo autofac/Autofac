8200b72afb2fd08be16db1cfc9b951dfae7fa858
sdhebert <unknown@kiln.example.com>
2010-08-23 21:49:45 +0500

    removed the abstract declaration on the attributedmetadatamodule because it is no longer required

--- contrib/Source/AutofacContrib.Attributed/AttributedMetadataModule.cs
+++ contrib/Source/AutofacContrib.Attributed/AttributedMetadataModule.cs
@@ -5,7 +5,7 @@ namespace AutofacContrib.Attributed
     /// <summary>
     /// this module will scan all registrations for metadata and associate them if found
     /// </summary>
-    public abstract class AttributedMetadataModule : Module
+    public class AttributedMetadataModule : Module
     {
         protected override void AttachToComponentRegistration(Autofac.Core.IComponentRegistry componentRegistry, Autofac.Core.IComponentRegistration registration)
         {
