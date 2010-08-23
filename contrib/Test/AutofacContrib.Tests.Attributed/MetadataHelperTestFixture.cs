6edeae568f506e8ae28e2afc8442f349f30565c1
sdhebert <unknown@kiln.example.com>
2010-08-23 19:11:37 +0500

    added the single attribute test case for the metadata discovery

--- contrib/Test/AutofacContrib.Tests.Attributed/MetadataHelperTestFixture.cs
+++ contrib/Test/AutofacContrib.Tests.Attributed/MetadataHelperTestFixture.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using AutofacContrib.Attributed;
 using AutofacContrib.Tests.Attributed.ScenarioTypes;
 using NUnit.Framework;
@@ -12,12 +9,22 @@ namespace AutofacContrib.Tests.Attributed
     public class MetadataHelperTestFixture
     {
         [Test]
-        public void scan_multiple_properties_into_one_enumerable_set()
+        public void scan_multiple_attributes_into_one_enumerable_set()
         {
             var metadata = MetadataHelper.GetMetadata(typeof (CombinationalWeakTypedScenario));
 
+            Assert.That(metadata.Count(), Is.EqualTo(2));
             Assert.That(metadata.Where(p => p.Key == "Name").FirstOrDefault().Value, Is.EqualTo("Hello"));
             Assert.That(metadata.Where(p => p.Key == "Age").FirstOrDefault().Value, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void scan_single_attribute_into_an_enumerable_set()
+        {
+            var metadata = MetadataHelper.GetMetadata(typeof (WeakTypedScenario));
+
+            Assert.That(metadata.Count(), Is.EqualTo(1));
+            Assert.That(metadata.Where(p => p.Key == "Name").FirstOrDefault().Value, Is.EqualTo("Hello"));
         }
     }
 }
