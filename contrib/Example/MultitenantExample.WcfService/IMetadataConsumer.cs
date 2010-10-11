cf17bb13362cc6d0a7c2e75d2bf8672ba3c2b618
travis.illig <unknown@kiln.example.com>
2010-10-12 01:15:56 +0700

    Added metadata consumer service.

---
+++ contrib/Example/MultitenantExample.WcfService/IMetadataConsumer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace MultitenantExample.WcfService
+{
+	[ServiceContract]
+	public interface IMetadataConsumer
+	{
+		[OperationContract]
+		GetServiceInfoResponse GetServiceInfo();
+	}
+}
