7a47d384eab4863562e772916f48075fad1bd185
brendan.kowitz <unknown@kiln.example.com>
2010-10-19 01:08:01 -1000

    changed input Expression to Func in MatchingScopeLifetime for WindowsPhone

--- src/Source/Autofac/Core/Lifetime/MatchingScopeLifetime.cs
+++ src/Source/Autofac/Core/Lifetime/MatchingScopeLifetime.cs
@@ -41,6 +41,7 @@ namespace Autofac.Core.Lifetime
         readonly Func<ILifetimeScope, bool> _matcher;
         readonly string _matchExpressionCode;
 
+#if !WINDOWS_PHONE
         /// <summary>
         /// Match scopes based on the provided expression.
         /// </summary>
@@ -51,6 +52,18 @@ namespace Autofac.Core.Lifetime
             _matcher = matchExpression.Compile();
             _matchExpressionCode = matchExpression.Body.ToString();
         }
+#else
+        /// <summary>
+        /// Match scopes based on the provided expression.
+        /// </summary>
+        /// <param name="matchExpression">Expression describing scopes that will match.</param>
+        public MatchingScopeLifetime(Func<ILifetimeScope, bool> matchExpression)
+        {
+            Enforce.ArgumentNotNull(matchExpression, "matchExpression");
+            _matcher = matchExpression;
+            _matchExpressionCode = matchExpression.Method.ToString();
+        }
+#endif
 
         /// <summary>
         /// Given the most nested scope visible within the resolve operation, find
