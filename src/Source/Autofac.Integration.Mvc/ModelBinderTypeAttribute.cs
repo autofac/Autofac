83697ad7fe0e314a8162450ed037da0f8144ff9a
alex.meyergleaves <unknown@kiln.example.com>
2010-12-12 03:49:11 -1000

    Upgraded integration to ASP.NET MVC 3 RC2. Added additional constructor to the ModelBinderTypeAttribute for CLS compliance.

--- src/Source/Autofac.Integration.Mvc/ModelBinderTypeAttribute.cs
+++ src/Source/Autofac.Integration.Mvc/ModelBinderTypeAttribute.cs
@@ -28,15 +28,35 @@ using System.Collections.Generic;
 
 namespace Autofac.Integration.Mvc
 {
+    /// <summary>
+    /// Indicates what types a model binder supports.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
     public sealed class ModelBinderTypeAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the target types.
+        /// </summary>
         public IEnumerable<Type> TargetTypes { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelBinderTypeAttribute"/> class.
+        /// </summary>
+        /// <param name="targetTypes">The target types.</param>
         public ModelBinderTypeAttribute(params Type[] targetTypes)
         {
             if (targetTypes == null) throw new ArgumentNullException("targetTypes");
             TargetTypes = targetTypes;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelBinderTypeAttribute"/> class.
+        /// </summary>
+        /// <param name="targetType">The target type.</param>
+        public ModelBinderTypeAttribute(Type targetType)
+        {
+            if (targetType == null) throw new ArgumentNullException("targetType");
+            TargetTypes = new Type[] {targetType};
         }
     }
 }
---
+++ src/Test/Autofac.Tests.Integration.Mvc/ModelBinderTypeAttributeFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac.Integration.Mvc;
+using NUnit.Framework;
+
+namespace Autofac.Tests.Integration.Mvc
+{
+    [TestFixture]
+    public class ModelBinderTypeAttributeFixture
+    {
+        [Test]
+        public void NullTargetTypeThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ModelBinderTypeAttribute((Type)null));
+        }
+
+        [Test]
+        public void NullTargetTypesThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ModelBinderTypeAttribute((Type[])null));
+        }
+    }
+}
