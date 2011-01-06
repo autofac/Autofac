f77bc05a9068d162a62756f3819a38dea046e2af
travis.illig <unknown@kiln.example.com>
2011-01-07 05:47:29 +0800

    Adding test stub I forgot in previous commit.

---
+++ contrib/Test/AutofacContrib.Tests.Multitenant/Stubs/StubDisposableDependency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AutofacContrib.Tests.Multitenant.Stubs
+{
+    public class StubDisposableDependency : IDisposable
+    {
+        // Intentionally a simple (and incorrect) disposable implementation.
+        // We need it for testing if Dispose was called, not actually to do
+        // the standard Dispose cleanup.
+
+        public bool IsDisposed = false;
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+    }
+}
