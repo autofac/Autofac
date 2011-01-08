c19dbfa9e107c808e20120c8d7ee2ce29cee442c
nicholas.blumhardt <unknown@kiln.example.com>
2011-01-08 13:58:25 -1000

    Added test for issue #282.

---
+++ src/Test/Autofac.Tests/Core/Registration/ExternalRegistrySourceTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Autofac.Tests.Core.Registration
+{
+    [TestFixture]
+    public class ExternalRegistrySourceTests
+    {
+        interface IServiceA { }
+        interface IServiceB { }
+        class ClassA : IServiceA, IServiceB { }
+        class ClassB : IServiceA { }
+
+        // Courtesy of M. Kowalewski
+        [Test, Ignore("Unresolved issue #282")]
+        public void OneTypeImplementTwoInterfaces_OtherObjectsImplementingOneOfThoseInterfaces_CanBeResolved()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType(typeof(ClassA)).As(typeof(IServiceA), typeof(IServiceB));
+            builder.RegisterType(typeof(ClassB)).As(typeof(IServiceA));
+
+            var container = builder.Build();
+            var lifetime = container.BeginLifetimeScope(cb => cb.Register(_ => new object()));
+            lifetime.Resolve<IServiceB>();
+
+            var allImplementationsOfServiceA = lifetime.Resolve<IEnumerable<IServiceA>>();
+            Assert.AreEqual(2, allImplementationsOfServiceA.Count());
+        }
+    }
+}
