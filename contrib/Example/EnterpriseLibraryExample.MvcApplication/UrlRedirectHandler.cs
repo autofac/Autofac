122b5c157572ffdd480bc6a7e3c008f8d0411f31
travis.illig <unknown@kiln.example.com>
2011-04-07 04:48:45 +0700

    Added example application for demonstrating EntLib configuration.

---
+++ contrib/Example/EnterpriseLibraryExample.MvcApplication/UrlRedirectHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Web;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling.Configuration;
+
+namespace EnterpriseLibraryExample.MvcApplication
+{
+    /// <summary>
+    /// Custom exception handler that redirects the user to a fixed URL.
+    /// </summary>
+    [ConfigurationElementType(typeof(CustomHandlerData))]
+    public class UrlRedirectHandler : IExceptionHandler
+    {
+        private string _url = null;
+
+        public UrlRedirectHandler(NameValueCollection configuration)
+        {
+            if (configuration != null)
+            {
+                this.ProcessConfiguration(configuration);
+            }
+        }
+
+        public Exception HandleException(Exception exception, Guid handlingInstanceId)
+        {
+            if (exception == null)
+            {
+                return null;
+            }
+            var context = HttpContext.Current;
+            if (context == null)
+            {
+                return exception;
+            }
+            context.ClearError();
+            context.Response.Redirect(this._url, true);
+            return exception;
+        }
+
+        private void ProcessConfiguration(NameValueCollection configuration)
+        {
+            this._url = configuration["url"];
+            if (String.IsNullOrEmpty(this._url))
+            {
+                throw new ConfigurationErrorsException("You must provide a URL to the URL redirect handler.");
+            }
+        }
+    }
+}
---
+++ contrib/Example/EnterpriseLibraryExample.MvcApplication/Views/Shared/Error.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <h2>
+        Sorry, an error occurred while processing your request.
+    </h2>
+</body>
+</html>
