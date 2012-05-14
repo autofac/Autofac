c7f9dd6d3f07710fba14a9c80c97c2e90ef93d58
travis.illig@gmail.com <unknown@kiln.example.com>
2012-05-15 01:10:27 +0700

    Moved contrib projects to the Extras folder and updated the project names. Where possible, references for Extras are switched to NuGet.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("AutofacContrib.DynamicProxy2")]
+[assembly: AssemblyTitle("Autofac.Extras.DynamicProxy2")]
 [assembly: AssemblyDescription("Autofac Castle.DynamicProxy2 Integration")]
