388f49a2c7fda1c6ff1340d0e138a304a589f082
travis.illig@gmail.com <unknown@kiln.example.com>
2012-05-18 01:31:00 +0700

    Split security attributes into separate shared/global AssemblyInfo file so unit test assemblies aren't marked with the same security attributes. The product assemblies need to run with the security rules enabled but writing unit tests with the appropriate security attributes and demands will be painful and isn't required.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
-using System.Security;
 
 [assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
 [assembly: AssemblyProduct("Autofac")]
@@ -9,14 +8,6 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Use .NET 4.0 security rules.
-[assembly: SecurityRules(SecurityRuleSet.Level2)]
-
-// All code defaults to SecurityTransparent but individual
-// types can be marked differently.
-// http://msdn.microsoft.com/en-us/library/dd233102.aspx
-[assembly: AllowPartiallyTrustedCallers]
 
 #if !WINDOWS_PHONE
 [assembly: ComVisible(false)]
---
+++ SecurityAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Security;
+
+// Use .NET 4.0 security rules.
+[assembly: SecurityRules(SecurityRuleSet.Level2)]
+
+// All code defaults to SecurityTransparent but individual
+// types can be marked differently.
+// http://msdn.microsoft.com/en-us/library/dd233102.aspx
+[assembly: AllowPartiallyTrustedCallers]
