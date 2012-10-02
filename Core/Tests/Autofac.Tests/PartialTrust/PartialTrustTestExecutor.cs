be06778f122dd0ce6487b6b3ca2b13c7bac69710
Travis@tillig-win8-vm.corp.checkfree.com <unknown@kiln.example.com>
2012-10-03 01:24:36 +0700

    Added partial trust test fixture.

---
+++ Core/Tests/Autofac.Tests/PartialTrust/PartialTrustTestExecutor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Web;
+using NUnit.Framework;
+
+namespace Autofac.Tests.PartialTrust
+{
+    [TestFixture]
+    public class PartialTrustTestExecutor
+    {
+        // Put the actual tests in the PartialTrustTests class as public void methods
+        // with no parameters. This fixture will automatically enumerate them and execute them
+        // in a partial trust domain.
+        private AppDomain _remoteDomain;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this._remoteDomain = CreateSandboxDomain();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AppDomain.Unload(this._remoteDomain);
+        }
+
+        [Test(Description = "Executes the partial trust tests.")]
+        [TestCaseSource("ExecutePartialTrustTestsSource")]
+        public void ExecutePartialTrustTests(MethodInfo testMethod)
+        {
+            var fixture = this.CreateRemoteFixture();
+            try
+            {
+                testMethod.Invoke(fixture, null);
+            }
+            catch (TargetInvocationException ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    throw ex.InnerException;
+                }
+                throw;
+            }
+        }
+
+        public IEnumerable<MethodInfo> ExecutePartialTrustTestsSource()
+        {
+            return
+                typeof(PartialTrustTests)
+                .GetMethods(BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.Public)
+                .Where(m => m.ReturnType == typeof(void) && m.ContainsGenericParameters == false && m.GetParameters().Length == 0);
+        }
+
+        private PartialTrustTests CreateRemoteFixture()
+        {
+            var assemblyName = typeof(PartialTrustTests).Assembly.FullName;
+            var typeName = typeof(PartialTrustTests).FullName;
+            var executor = (PartialTrustTests)this._remoteDomain.CreateInstanceAndUnwrap(assemblyName, typeName);
+            return executor;
+        }
+
+        private static AppDomain CreateSandboxDomain()
+        {
+            // Normally from a security perspective we'd put the sandboxed app in its own
+            // base directory, but to make things easier (so we don't have to copy the NUnit
+            // assembly, etc.) we'll just mirror the current test domain settings.
+            var info = new AppDomainSetup()
+            {
+                ApplicationBase = AppDomain.CurrentDomain.BaseDirectory,
+                PrivateBinPath = AppDomain.CurrentDomain.RelativeSearchPath
+            };
+
+            // Grant set is the same set of permissions as ASP.NET medium trust EXCEPT
+            // it excludes the FileIOPermission, IsolatedStorageFilePermission, and PrintingPermission.
+            var grantSet = new PermissionSet(null);
+            grantSet.AddPermission(new AspNetHostingPermission(AspNetHostingPermissionLevel.Medium));
+            grantSet.AddPermission(new DnsPermission(PermissionState.Unrestricted));
+            grantSet.AddPermission(new EnvironmentPermission(EnvironmentPermissionAccess.Read, "TEMP;TMP;USERNAME;OS;COMPUTERNAME"));
+            grantSet.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution | SecurityPermissionFlag.ControlThread | SecurityPermissionFlag.ControlPrincipal | SecurityPermissionFlag.RemotingConfiguration));
+            grantSet.AddPermission(new SmtpPermission(SmtpAccess.Connect));
+            grantSet.AddPermission(new SqlClientPermission(PermissionState.Unrestricted));
+            grantSet.AddPermission(new TypeDescriptorPermission(PermissionState.Unrestricted));
+            grantSet.AddPermission(new WebPermission(PermissionState.Unrestricted));
+            grantSet.AddPermission(new ReflectionPermission(ReflectionPermissionFlag.RestrictedMemberAccess));
+
+            return AppDomain.CreateDomain("Sandbox", null, info, grantSet);
+        }
+    }
+}
---
+++ Core/Tests/Autofac.Tests/PartialTrust/PartialTrustTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using Autofac.Tests.Scenarios.Graph1;
+using NUnit.Framework;
+
+namespace Autofac.Tests.PartialTrust
+{
+    /// <summary>
+    /// Fixture containing the set of tests that will execute in partial trust.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// These tests are not marked with any NUnit attributes because they actually get executed
+    /// through NUnit via the <see cref="Autofac.Tests.PartialTrust.PartialTrustTestExecutor"/>.
+    /// Any public void method with no parameters found here will execute as a unit test.
+    /// </para>
+    /// </remarks>
+    public class PartialTrustTests : MarshalByRefObject
+    {
+        public void AppDomainSetupCorrect()
+        {
+            // The sandbox should have the expected name and we shouldn't be able to demand unrestricted permissions.
+            Assert.AreEqual("Sandbox", AppDomain.CurrentDomain.FriendlyName, "The AppDomain friendly name was not correct.");
+            Assert.Throws<SecurityException>(() => new SecurityPermission(PermissionState.Unrestricted).Demand());
+        }
+
+        //public void Integration_CanCorrectlyBuildGraph1()
+        //{
+        //    var builder = new ContainerBuilder();
+
+        //    builder.RegisterType<A1>().SingleInstance();
+        //    builder.RegisterType<CD1>().As<IC1, ID1>().SingleInstance();
+        //    builder.RegisterType<E1>().SingleInstance();
+        //    builder.Register(ctr => new B1(ctr.Resolve<A1>()));
+
+        //    var target = builder.Build();
+
+        //    E1 e = target.Resolve<E1>();
+        //    A1 a = target.Resolve<A1>();
+        //    B1 b = target.Resolve<B1>();
+        //    IC1 c = target.Resolve<IC1>();
+        //    ID1 d = target.Resolve<ID1>();
+
+        //    Assert.IsInstanceOf<CD1>(c);
+        //    CD1 cd = (CD1)c;
+
+        //    Assert.AreSame(a, b.A);
+        //    Assert.AreSame(a, cd.A);
+        //    Assert.AreNotSame(b, cd.B);
+        //    Assert.AreSame(c, e.C);
+        //    Assert.AreNotSame(b, e.B);
+        //    Assert.AreNotSame(e.B, cd.B);
+        //}
+
+        //public interface I1<T> { }
+        //public interface I2<T> { }
+        //public class C<T> : I1<T>, I2<T> { }
+
+        //public void Integration_MultipleServicesOnAnOpenGenericType_ShareTheSameRegistration()
+        //{
+        //    var builder = new ContainerBuilder();
+        //    builder.RegisterGeneric(typeof(C<>)).As(typeof(I1<>), typeof(I2<>));
+        //    var container = builder.Build();
+        //    container.Resolve<I1<int>>();
+        //    var count = container.ComponentRegistry.Registrations.Count();
+        //    container.Resolve<I2<int>>();
+        //    Assert.AreEqual(count, container.ComponentRegistry.Registrations.Count());
+        //}
+    }
+}
