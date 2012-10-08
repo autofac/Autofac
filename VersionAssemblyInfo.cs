6118db853da07a052fe8ec8a5c2315fb606877e8
alex.meyergleaves <unknown@kiln.example.com>
2012-10-08 01:39:04 -1100

    Updated build script to work with NuGet 2.1 support for PCL.

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyConfiguration("Release built on 2012-10-08 00:42")]
+[assembly: AssemblyConfiguration("Release built on 2012-10-08 23:36")]
 
 
