1133cd20bcfd8327a0b2967edc9dba4a20fe9969
Travis@tillig-win8-vm <unknown@kiln.example.com>
2012-12-04 01:42:26 +0800

    Switched Autofac.Extras.CommonServiceLocator to a portable class library.

--- Extras/Source/Autofac.Extras.CommonServiceLocator/AutofacServiceLocator.cs
+++ Extras/Source/Autofac.Extras.CommonServiceLocator/AutofacServiceLocator.cs
@@ -7,28 +7,72 @@ using Microsoft.Practices.ServiceLocation;
 
 namespace Autofac.Extras.CommonServiceLocator
 {
+    /// <summary>
+    /// Autofac implementation of the Microsoft CommonServiceLocator.
+    /// </summary>
     public sealed class AutofacServiceLocator : ServiceLocatorImplBase
     {
-        readonly IComponentContext _container;
+        /// <summary>
+        /// The <see cref="Autofac.IComponentContext"/> from which services
+        /// should be located.
+        /// </summary>
+        private readonly IComponentContext _container;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Autofac.Extras.CommonServiceLocator.AutofacServiceLocator" /> class.
+        /// </summary>
+        /// <param name="container">
+        /// The <see cref="Autofac.IComponentContext"/> from which services
+        /// should be located.
+        /// </param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if <paramref name="container" /> is <see langword="null" />.
+        /// </exception>
         public AutofacServiceLocator(IComponentContext container)
         {
             if (container == null)
+            {
                 throw new ArgumentNullException("container");
+            }
             _container = container;
         }
 
+        /// <summary>
+        /// Resolves the requested service instance.
+        /// </summary>
+        /// <param name="serviceType">Type of instance requested.</param>
+        /// <param name="key">Name of registered service you want. May be <see langword="null" />.</param>
+        /// <returns>The requested service instance.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if <paramref name="serviceType" /> is <see langword="null" />.
+        /// </exception>
         protected override object DoGetInstance(Type serviceType, string key)
         {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException("serviceType");
+            }
             return key != null ? _container.ResolveNamed(key, serviceType) : _container.Resolve(serviceType);
         }
 
+        /// <summary>
+        /// Resolves all requested service instances.
+        /// </summary>
+        /// <param name="serviceType">Type of instance requested.</param>
+        /// <returns>Sequence of service instance objects.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if <paramref name="serviceType" /> is <see langword="null" />.
+        /// </exception>
         protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
         {
-            var enumerableType = typeof (IEnumerable<>).MakeGenericType(serviceType);
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException("serviceType");
+            }
+            var enumerableType = typeof(IEnumerable<>).MakeGenericType(serviceType);
 
             object instance = _container.Resolve(enumerableType);
-            return ((IEnumerable) instance).Cast<object>();
+            return ((IEnumerable)instance).Cast<object>();
         }
     }
 }
