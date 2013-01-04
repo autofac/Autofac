c3af78611b04b2aaf15fe0a99453343259b7b6db
alex.meyergleaves <unknown@kiln.example.com>
2013-01-04 02:24:37 -1000

    Version information for Beta 2 build.

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyConfiguration("Release built on 2012-11-01 22:56")]
+[assembly: AssemblyConfiguration("Release built on 2012-12-23 11:27")]
 
 
