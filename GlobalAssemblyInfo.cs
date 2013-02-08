caf95c4b4e2a0c293d4eaadef62dae0e1a20c682
alex.meyergleaves <unknown@kiln.example.com>
2013-02-07 22:55:25 -1000

    Updated build script to allow separate file and assembly versions. The file version is now used for NuGet semver and zip package names. This will allow for Minor and Patch releases without the assembly identity changing due to the strong name. The build script also writes out the copyright using the current year.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Resources;
 
 [assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
 [assembly: AssemblyProduct("Autofac")]
-[assembly: AssemblyCopyright("Copyright © 2011 Autofac Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
