02e3c43d6441f1941666aa9bf8ea798d2d39f433
alex.meyergleaves <unknown@kiln.example.com>
2013-02-13 02:52:24 -1000

    Added a RegisterHubs extension method for the SignalR integration.

---
+++ Core/Source/Autofac.Integration.SignalR/RegistrationExtensions.cs
@@ -0,0 +1,51 @@
+﻿// This software is part of the Autofac IoC container
+// Copyright © 2013 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System.Reflection;
+using Autofac.Builder;
+using Autofac.Features.Scanning;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Autofac.Integration.SignalR
+{
+    /// <summary>
+    /// Extends <see cref="ContainerBuilder"/> with methods to support ASP.NET SignalR.
+    /// </summary>
+    public static class RegistrationExtensions
+    {
+        /// <summary>
+        /// Register types that implement <see cref="IHub"/> in the provided assemblies.
+        /// </summary>
+        /// <param name="builder">The container builder.</param>
+        /// <param name="controllerAssemblies">Assemblies to scan for controllers.</param>
+        /// <returns>Registration builder allowing the controller components to be customised.</returns>
+        public static IRegistrationBuilder<object, ScanningActivatorData, DynamicRegistrationStyle>
+            RegisterHubs(this ContainerBuilder builder, params Assembly[] controllerAssemblies)
+        {
+            return builder.RegisterAssemblyTypes(controllerAssemblies)
+                .Where(t => typeof(IHub).IsAssignableFrom(t));
+        }
+    }
+}
---
+++ Core/Tests/Autofac.Tests.Integration.SignalR/RegistrationExtensionsFixture.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using NUnit.Framework;
+using Autofac.Integration.SignalR;
+
+namespace Autofac.Tests.Integration.SignalR
+{
+    [TestFixture]
+    public class RegistrationExtensionsFixture
+    {
+        [Test]
+        public void RegisterHubsFindHubInterfaces()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterHubs(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+
+            Assert.That(container.IsRegistered<TestHub>(), Is.True);
+        }
+    }
+
+    public class TestHub : Hub
+    {
+    }
+}
