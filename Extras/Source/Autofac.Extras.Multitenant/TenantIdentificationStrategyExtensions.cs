ca0fdb522ed81eb57f6114bbf169de42426ea136
Travis@tillig-win8-vm <unknown@kiln.example.com>
2013-03-22 05:44:53 +0700

    Resolved Issue #402: Added extension method for easy tenant ID retrieval.

---
+++ Extras/Source/Autofac.Extras.Multitenant/TenantIdentificationStrategyExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Autofac.Extras.Multitenant
+{
+    /// <summary>
+    /// Extension methods for working with <see cref="Autofac.Extras.Multitenant.ITenantIdentificationStrategy"/>.
+    /// </summary>
+    public static class TenantIdentificationStrategyExtensions
+    {
+        /// <summary>
+        /// Gets a typed tenant ID from a strategy or the default value for the type
+        /// if identification fails.
+        /// </summary>
+        /// <typeparam name="T">The type of the tenant ID.</typeparam>
+        /// <param name="strategy">
+        /// The <see cref="Autofac.Extras.Multitenant.ITenantIdentificationStrategy"/> from which the tenant ID should be retrieved.
+        /// </param>
+        /// <returns>
+        /// If tenant identification succeeds, the ID from <paramref name="strategy" /> is converted to
+        /// <typeparamref name="T"/> and returned. If identification fails, the default value for
+        /// <typeparamref name="T"/> is returned.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if <paramref name="strategy" /> is <see langword="null" />.
+        /// </exception>
+        public static T IdentifyTenant<T>(this ITenantIdentificationStrategy strategy)
+        {
+            if (strategy == null)
+            {
+                throw new ArgumentNullException("strategy");
+            }
+            object id;
+            if (strategy.TryIdentifyTenant(out id))
+            {
+                return (T)id;
+            }
+            return default(T);
+        }
+    }
+}
---
+++ Extras/Tests/Autofac.Extras.Tests.Multitenant/TenantIdentificationStrategyExtensionsFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Autofac;
+using Autofac.Extras.Multitenant;
+using Autofac.Extras.Tests.Multitenant.Stubs;
+using NUnit.Framework;
+
+namespace Autofac.Extras.Tests.Multitenant
+{
+    [TestFixture]
+    public class TenantIdentificationStrategyExtensionsFixture
+    {
+        [Test(Description = "Retrieves the tenant ID but tries to convert to the wrong type.")]
+        public void IdentifyTenant_FailedConversion()
+        {
+            var strategy = new StubTenantIdentificationStrategy
+            {
+                TenantId = Guid.NewGuid()
+            };
+            Assert.Throws<InvalidCastException>(() => strategy.IdentifyTenant<int>());
+        }
+
+        [Test(Description = "Attempts to retrieve the tenant ID when identification fails.")]
+        public void IdentifyTenant_FailedRetrieval()
+        {
+            var strategy = new StubTenantIdentificationStrategy
+            {
+                IdentificationSuccess = false
+            };
+            Assert.AreEqual(Guid.Empty, strategy.IdentifyTenant<Guid>(), "The tenant ID should be the default for the type if identification fails.");
+        }
+
+        [Test(Description = "Attempts to get the tenant ID from a null strategy.")]
+        public void IdentifyTenant_NullStrategy()
+        {
+            ITenantIdentificationStrategy strategy = null;
+            Assert.Throws<ArgumentNullException>(() => strategy.IdentifyTenant<Guid>());
+        }
+
+        [Test(Description = "Successfully retrieves and converts the tenant ID.")]
+        public void IdentifyTenant_SuccessfulRetrieval()
+        {
+            var expected = Guid.NewGuid();
+            var strategy = new StubTenantIdentificationStrategy
+            {
+                TenantId = expected
+            };
+            Assert.AreEqual(expected, strategy.IdentifyTenant<Guid>(), "The tenant ID wasn't properly retrieved and parsed.");
+        }
+    }
+}
