ae8c5336ea6eae30211b2f0dec3377a801f32beb
alexmg <alex.meyergleaves@gmail.com>
2014-02-10 22:51:26 +1000

    Added project, unit tests and .nuspec for Autofac.Mvc5.Owin package.

---
+++ Core/Tests/Autofac.Tests.Integration.WebApi.Mvc/OwinExtensionsFixture.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using Autofac.Integration.Mvc.Owin;
+using Autofac.Integration.Owin;
+using Autofac.Tests.Integration.Owin;
+using Microsoft.Owin.Testing;
+using Moq;
+using NUnit.Framework;
+using Owin;
+using OwinExtensions = Autofac.Integration.Mvc.Owin.OwinExtensions;
+
+namespace Autofac.Tests.Integration.Mvc.Owin
+{
+    [TestFixture]
+    public class OwinExtensionsFixture
+    {
+        [Test]
+        public void UseAutofacMvcUpdatesHttpContextWithLifetimeScopeFromOwinContext()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<TestMiddleware>();
+            var container = builder.Build();
+
+            var httpContext = new Mock<HttpContextBase>();
+            httpContext.SetupSet(mock => mock.Items[typeof(ILifetimeScope)] = It.IsAny<ILifetimeScope>());
+            OwinExtensions.CurrentHttpContext = () => httpContext.Object;
+
+            using (var server = TestServer.Create(app =>
+            {
+                app.UseAutofacMiddleware(container);
+                app.UseAutofacMvc();
+                app.Run(context => context.Response.WriteAsync("Hello, world!"));
+            }))
+            {
+                server.HttpClient.GetAsync("/").Wait();
+                httpContext.VerifyAll();
+            }
+        }
+    }
+}
