8db64bf32b77762049bd8e4910481f5500be3b88
Ivan Ferić <ivan.feric@gmail.com>
2014-03-17 13:39:39 +0100

    Update InterceptTransparentProxyFixture.cs
    Updated tests

--- InterceptTransparentProxyFixture.cs
+++ InterceptTransparentProxyFixture.cs
@@ -1,141 +1,168 @@
-﻿using System;
-using System.ServiceModel;
-using System.ServiceModel.Description;
-using Autofac.Core;
-using Autofac.Extras.DynamicProxy2;
-using Autofac.Integration.Wcf;
 using Castle.DynamicProxy;
 using NUnit.Framework;
+using Autofac;
+using Autofac.Extras.DynamicProxy2;
+using System.Linq;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace Autofac.Extras.Tests.DynamicProxy2
 {
     [TestFixture]
-    public class InterceptTransparentProxyFixture
+    public class InterceptorsChosenByMetadataFixture
     {
-        private static readonly Uri TestServiceAddress = new Uri("http://localhost:80/Temporary_Listen_Addresses/ITestService");
-
-        [Test(Description = "The service being intercepted must be registered as an interface.")]
-        public void ServiceMustBeInterface()
+        public interface ICustomerService
         {
-            var builder = new ContainerBuilder();
-            builder.Register(c => new object()).InterceptTransparentProxy();
-            var container = builder.Build();
-
-            Assert.Throws<DependencyResolutionException>(() => container.Resolve<object>());
+            int GetVisitCount();
+			int GetUniqueVisitorCount();
         }
 
-        [Test(Description = "The instance being intercepted must be a transparent proxy.")]
-        public void ServiceMustBeTransparentProxy()
+        public class CustomerService : ICustomerService
         {
-            var builder = new ContainerBuilder();
-            builder.Register(c => new object()).As<ITestService>().InterceptTransparentProxy();
-            var container = builder.Build();
+            int VisitCount { get; set; }
+			int UniqueVisitorCount { get; set; }
 
-            var exception = Assert.Throws<DependencyResolutionException>(() => container.Resolve<ITestService>());
-
-            Assert.That(exception.Message, Is.StringContaining(typeof(object).FullName));
-        }
-
-        [Test(Description = "The instance must implement the additional interfaces provided.")]
-        public void ProxyMustImplementAdditionalInterfaces()
-        {
-            var builder = new ContainerBuilder();
-            builder.Register(c => CreateChannelFactory()).SingleInstance();
-            builder.Register(c => c.Resolve<ChannelFactory<ITestService>>().CreateChannel())
-                .SingleInstance()
-                .InterceptTransparentProxy(typeof(ICloneable), typeof(IFormattable));
-            
-            var container = builder.Build();
-
-            var exception = Assert.Throws<DependencyResolutionException>(() => container.Resolve<ITestService>());
-
-            Assert.That(exception.Message, Is.StringContaining(typeof(ICloneable).FullName));
-            Assert.That(exception.Message, Is.StringContaining(typeof(IFormattable).FullName));
-        }
-
-        [Test(Description = "Issue 361: WCF service client code should allow interception to occur.")]
-        public void ServiceClientInterceptionIsPossible()
-        {
-            // Build the service-side container
-            var sb = new ContainerBuilder();
-            sb.RegisterType<TestService>().As<ITestService>();
-
-            // Build the client-side container with interception
-            // around the client proxy. Issue 361 was that there
-            // seemed to be trouble around getting this to work.
-            var cb = new ContainerBuilder();
-            cb.RegisterType<TestServiceInterceptor>();
-            cb.Register(c => CreateChannelFactory()).SingleInstance();
-            cb
-                .Register(c => c.Resolve<ChannelFactory<ITestService>>().CreateChannel())
-                .InterceptTransparentProxy(typeof(IClientChannel))
-                .InterceptedBy(typeof(TestServiceInterceptor))
-                .UseWcfSafeRelease();
-
-            using (var sc = sb.Build())
+            public CustomerService()
             {
-                // Start the self-hosted test service
-                var host = CreateTestServiceHost(sc);
-                host.Open();
-                try
-                {
-                    using (var cc = cb.Build())
-                    {
-                        // Make a call through the client to the service -
-                        // it should be intercepted.
-                        var client = cc.Resolve<ITestService>();
-                        Assert.AreEqual("interceptor", client.DoWork(), "The call through the client proxy to the service was not intercepted.");
-                    }
-                }
-                finally
-                {
-                    host.Close();
-                }
+                VisitCount = 10;
+				UniqueVisitorCount = 6;
             }
-        }
 
-        private static ServiceHost CreateTestServiceHost(ILifetimeScope container)
-        {
-            var host = new ServiceHost(typeof(TestService), TestServiceAddress);
-            host.AddServiceEndpoint(typeof(ITestService), new BasicHttpBinding(), "");
-            host.AddDependencyInjectionBehavior<ITestService>(container);
-            host.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true, HttpGetUrl = TestServiceAddress });
-            return host;
-        }
-
-        static ChannelFactory<ITestService> CreateChannelFactory()
-        {
-            return new ChannelFactory<ITestService>(new BasicHttpBinding(), new EndpointAddress(TestServiceAddress));
-        }
-
-        [ServiceContract]
-        public interface ITestService
-        {
-            [OperationContract]
-            string DoWork();
-        }
-
-        public class TestService : ITestService
-        {
-            public string DoWork()
+            public int GetVisitCount()
             {
-                return "service";
+                return VisitCount;
             }
+
+			public int GetUniqueVisitorCount()
+			{
+				return UniqueVisitorCount;
+			}
         }
 
-        public class TestServiceInterceptor : IInterceptor
+        class AddOneInterceptor : IInterceptor
         {
             public void Intercept(IInvocation invocation)
             {
-                if (invocation.Method.Name == "DoWork")
-                {
-                    invocation.ReturnValue = "interceptor";
-                }
-                else
-                {
-                    invocation.Proceed();
-                }
+                invocation.Proceed();
+                if (invocation.Method.Name.StartsWith("Get"))
+                    invocation.ReturnValue = 1 + (int)invocation.ReturnValue;
             }
         }
+
+		class AddTenInterceptor : IInterceptor
+		{
+			public void Intercept(IInvocation invocation)
+			{
+				invocation.Proceed();
+				if (invocation.Method.Name == "GetUniqueVisitorCount")
+					invocation.ReturnValue = 10 + (int)invocation.ReturnValue;
+			}
+		}
+
+		class InterceptOnlyUnique : IProxyGenerationHook
+		{
+
+			public void MethodsInspected()
+			{
+			}
+
+			public void NonProxyableMemberNotification(Type type, MemberInfo memberInfo)
+			{
+			}
+
+			public bool ShouldInterceptMethod(Type type, MethodInfo methodInfo)
+			{
+				return methodInfo.Name.Equals("GetUniqueVisitorCount");
+			}
+
+		}
+
+		class MyInterceptorSelector : IInterceptorSelector
+		{
+			public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+			{
+				return method.Name == "GetVisitCount"
+					? interceptors.OfType<AddOneInterceptor>().ToArray<IInterceptor>()
+					: interceptors.OfType<AddTenInterceptor>().ToArray<IInterceptor>();
+			}
+		}
+
+        [Test]
+        public void InterceptsWhenUsingExtendedPropertyAndType()
+		{
+            var builder = new ContainerBuilder();
+            builder.RegisterType<CustomerService>()
+                .As<ICustomerService>()
+                .EnableInterfaceInterceptors()
+                .InterceptedBy(typeof(AddOneInterceptor));
+            builder.RegisterType<AddOneInterceptor>();
+            var container = builder.Build();
+            var cs = container.Resolve<ICustomerService>();
+            Assert.AreEqual(11, cs.GetVisitCount());
+        }
+
+		[Test]
+		public void InterceptsWithMixinWhenUsingExtendedPropertyAndType()
+		{
+			var options = new ProxyGenerationOptions();
+			options.AddMixinInstance(new Dictionary<int, int>());
+
+			var builder = new ContainerBuilder();
+			builder.RegisterType<CustomerService>()
+				.As<ICustomerService>()
+				.EnableInterfaceInterceptors(options);
+			var container = builder.Build();
+
+			var cs = container.Resolve<ICustomerService>();
+			var dict = cs as IDictionary<int, int>;
+
+			Assert.IsNotNull(dict);
+
+			dict.Add(1, 2);
+
+			Assert.AreEqual(2, dict[1]);
+
+			dict.Clear();
+
+			Assert.IsEmpty(dict);
+		}
+
+		[Test]
+		public void CanInterceptOnlySpecificMethods()
+		{
+			var options = new ProxyGenerationOptions(new InterceptOnlyUnique());
+
+			var builder = new ContainerBuilder();
+			builder.RegisterType<CustomerService>()
+				.As<ICustomerService>()
+				.EnableInterfaceInterceptors(options)
+				.InterceptedBy(typeof(AddOneInterceptor), typeof(AddTenInterceptor));
+			builder.RegisterType<AddOneInterceptor>();
+			builder.RegisterType<AddTenInterceptor>();
+			var cpt = builder.Build().Resolve<ICustomerService>();
+
+			Assert.AreEqual(10, cpt.GetVisitCount());
+			Assert.AreEqual(17, cpt.GetUniqueVisitorCount());
+		}
+
+		[Test]
+		public void CanInterceptMethodsWithSpecificInterceptors()
+		{
+			var options = new ProxyGenerationOptions { Selector = new MyInterceptorSelector() };
+
+			var builder = new ContainerBuilder();
+			builder.RegisterType<CustomerService>()
+				.As<ICustomerService>()
+				.EnableInterfaceInterceptors(options)
+				.InterceptedBy(typeof(AddOneInterceptor), typeof(AddTenInterceptor));
+			builder.RegisterType<AddOneInterceptor>();
+			builder.RegisterType<AddTenInterceptor>();
+			var cpt = builder.Build().Resolve<ICustomerService>();
+
+			Assert.AreEqual(11, cpt.GetVisitCount());
+			Assert.AreEqual(16, cpt.GetUniqueVisitorCount());
+		}
+
     }
 }
