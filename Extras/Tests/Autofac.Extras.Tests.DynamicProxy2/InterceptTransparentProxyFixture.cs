35e786908bcfa7183398c49a14abc1519e2e41cb
Ivan Ferić <ivan.feric@gmail.com>
2014-03-17 13:46:30 +0100

    Update InterceptTransparentProxyFixture.cs
    Updated tests

--- Extras/Tests/Autofac.Extras.Tests.DynamicProxy2/InterceptTransparentProxyFixture.cs
+++ Extras/Tests/Autofac.Extras.Tests.DynamicProxy2/InterceptTransparentProxyFixture.cs
@@ -15,18 +15,18 @@ namespace Autofac.Extras.Tests.DynamicProxy2
         public interface ICustomerService
         {
             int GetVisitCount();
-			int GetUniqueVisitorCount();
+            int GetUniqueVisitorCount();
         }
 
         public class CustomerService : ICustomerService
         {
             int VisitCount { get; set; }
-			int UniqueVisitorCount { get; set; }
+            int UniqueVisitorCount { get; set; }
 
             public CustomerService()
             {
                 VisitCount = 10;
-				UniqueVisitorCount = 6;
+                UniqueVisitorCount = 6;
             }
 
             public int GetVisitCount()
@@ -34,10 +34,10 @@ namespace Autofac.Extras.Tests.DynamicProxy2
                 return VisitCount;
             }
 
-			public int GetUniqueVisitorCount()
-			{
-				return UniqueVisitorCount;
-			}
+            public int GetUniqueVisitorCount()
+            {
+                return UniqueVisitorCount;
+            }
         }
 
         class AddOneInterceptor : IInterceptor
@@ -50,47 +50,46 @@ namespace Autofac.Extras.Tests.DynamicProxy2
             }
         }
 
-		class AddTenInterceptor : IInterceptor
-		{
-			public void Intercept(IInvocation invocation)
-			{
-				invocation.Proceed();
-				if (invocation.Method.Name == "GetUniqueVisitorCount")
-					invocation.ReturnValue = 10 + (int)invocation.ReturnValue;
-			}
-		}
+        class AddTenInterceptor : IInterceptor
+        {
+            public void Intercept(IInvocation invocation)
+            {
+                invocation.Proceed();
+                if (invocation.Method.Name == "GetUniqueVisitorCount")
+                    invocation.ReturnValue = 10 + (int)invocation.ReturnValue;
+            }
+        }
 
-		class InterceptOnlyUnique : IProxyGenerationHook
-		{
+        class InterceptOnlyUnique : IProxyGenerationHook
+        {
+            public void MethodsInspected()
+            {
+            }
 
-			public void MethodsInspected()
-			{
-			}
+            public void NonProxyableMemberNotification(Type type, MemberInfo memberInfo)
+            {
+            }
 
-			public void NonProxyableMemberNotification(Type type, MemberInfo memberInfo)
-			{
-			}
+            public bool ShouldInterceptMethod(Type type, MethodInfo methodInfo)
+            {
+                return methodInfo.Name.Equals("GetUniqueVisitorCount");
+            }
 
-			public bool ShouldInterceptMethod(Type type, MethodInfo methodInfo)
-			{
-				return methodInfo.Name.Equals("GetUniqueVisitorCount");
-			}
+        }
 
-		}
-
-		class MyInterceptorSelector : IInterceptorSelector
-		{
-			public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
-			{
-				return method.Name == "GetVisitCount"
-					? interceptors.OfType<AddOneInterceptor>().ToArray<IInterceptor>()
-					: interceptors.OfType<AddTenInterceptor>().ToArray<IInterceptor>();
-			}
-		}
+        class MyInterceptorSelector : IInterceptorSelector
+        {
+            public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+            {
+                return method.Name == "GetVisitCount"
+                    ? interceptors.OfType<AddOneInterceptor>().ToArray<IInterceptor>()
+                    : interceptors.OfType<AddTenInterceptor>().ToArray<IInterceptor>();
+            }
+        }
 
         [Test]
         public void InterceptsWhenUsingExtendedPropertyAndType()
-		{
+        {
             var builder = new ContainerBuilder();
             builder.RegisterType<CustomerService>()
                 .As<ICustomerService>()
@@ -102,67 +101,67 @@ namespace Autofac.Extras.Tests.DynamicProxy2
             Assert.AreEqual(11, cs.GetVisitCount());
         }
 
-		[Test]
-		public void InterceptsWithMixinWhenUsingExtendedPropertyAndType()
-		{
-			var options = new ProxyGenerationOptions();
-			options.AddMixinInstance(new Dictionary<int, int>());
+        [Test]
+        public void InterceptsWithMixinWhenUsingExtendedPropertyAndType()
+        {
+            var options = new ProxyGenerationOptions();
+            options.AddMixinInstance(new Dictionary<int, int>());
 
-			var builder = new ContainerBuilder();
-			builder.RegisterType<CustomerService>()
-				.As<ICustomerService>()
-				.EnableInterfaceInterceptors(options);
-			var container = builder.Build();
+            var builder = new ContainerBuilder();
+            builder.RegisterType<CustomerService>()
+                .As<ICustomerService>()
+                .EnableInterfaceInterceptors(options);
+            var container = builder.Build();
 
-			var cs = container.Resolve<ICustomerService>();
-			var dict = cs as IDictionary<int, int>;
+            var cs = container.Resolve<ICustomerService>();
+            var dict = cs as IDictionary<int, int>;
 
-			Assert.IsNotNull(dict);
+            Assert.IsNotNull(dict);
 
-			dict.Add(1, 2);
+            dict.Add(1, 2);
 
-			Assert.AreEqual(2, dict[1]);
+            Assert.AreEqual(2, dict[1]);
 
-			dict.Clear();
+            dict.Clear();
 
-			Assert.IsEmpty(dict);
-		}
+            Assert.IsEmpty(dict);
+        }
 
-		[Test]
-		public void CanInterceptOnlySpecificMethods()
-		{
-			var options = new ProxyGenerationOptions(new InterceptOnlyUnique());
+        [Test]
+        public void CanInterceptOnlySpecificMethods()
+        {
+            var options = new ProxyGenerationOptions(new InterceptOnlyUnique());
 
-			var builder = new ContainerBuilder();
-			builder.RegisterType<CustomerService>()
-				.As<ICustomerService>()
-				.EnableInterfaceInterceptors(options)
-				.InterceptedBy(typeof(AddOneInterceptor), typeof(AddTenInterceptor));
-			builder.RegisterType<AddOneInterceptor>();
-			builder.RegisterType<AddTenInterceptor>();
-			var cpt = builder.Build().Resolve<ICustomerService>();
+            var builder = new ContainerBuilder();
+            builder.RegisterType<CustomerService>()
+                .As<ICustomerService>()
+                .EnableInterfaceInterceptors(options)
+                .InterceptedBy(typeof(AddOneInterceptor), typeof(AddTenInterceptor));
+            builder.RegisterType<AddOneInterceptor>();
+            builder.RegisterType<AddTenInterceptor>();
+            var cpt = builder.Build().Resolve<ICustomerService>();
 
-			Assert.AreEqual(10, cpt.GetVisitCount());
-			Assert.AreEqual(17, cpt.GetUniqueVisitorCount());
-		}
+            Assert.AreEqual(10, cpt.GetVisitCount());
+            Assert.AreEqual(17, cpt.GetUniqueVisitorCount());
+        }
 
-		[Test]
-		public void CanInterceptMethodsWithSpecificInterceptors()
-		{
-			var options = new ProxyGenerationOptions { Selector = new MyInterceptorSelector() };
+        [Test]
+        public void CanInterceptMethodsWithSpecificInterceptors()
+        {
+            var options = new ProxyGenerationOptions { Selector = new MyInterceptorSelector() };
 
-			var builder = new ContainerBuilder();
-			builder.RegisterType<CustomerService>()
-				.As<ICustomerService>()
-				.EnableInterfaceInterceptors(options)
-				.InterceptedBy(typeof(AddOneInterceptor), typeof(AddTenInterceptor));
-			builder.RegisterType<AddOneInterceptor>();
-			builder.RegisterType<AddTenInterceptor>();
-			var cpt = builder.Build().Resolve<ICustomerService>();
+            var builder = new ContainerBuilder();
+            builder.RegisterType<CustomerService>()
+                .As<ICustomerService>()
+                .EnableInterfaceInterceptors(options)
+                .InterceptedBy(typeof(AddOneInterceptor), typeof(AddTenInterceptor));
+            builder.RegisterType<AddOneInterceptor>();
+            builder.RegisterType<AddTenInterceptor>();
+            var cpt = builder.Build().Resolve<ICustomerService>();
 
-			Assert.AreEqual(11, cpt.GetVisitCount());
-			Assert.AreEqual(16, cpt.GetUniqueVisitorCount());
-		}
+            Assert.AreEqual(11, cpt.GetVisitCount());
+            Assert.AreEqual(16, cpt.GetUniqueVisitorCount());
+        }
 
     }
 }
