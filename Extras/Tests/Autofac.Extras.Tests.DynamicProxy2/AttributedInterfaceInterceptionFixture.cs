74fe0a656bd97ae5f441d6584a5bd45242f59096
Ivan Ferić <ivan.feric@gmail.com>
2014-03-27 13:40:57 +0100

    Update AttributedInterfaceInterceptionFixture.cs
    Reverted to original tests

--- Extras/Tests/Autofac.Extras.Tests.DynamicProxy2/AttributedInterfaceInterceptionFixture.cs
+++ Extras/Tests/Autofac.Extras.Tests.DynamicProxy2/AttributedInterfaceInterceptionFixture.cs
@@ -1,12 +1,9 @@
-using System;
+?using System;
 using Autofac;
 using Autofac.Core;
 using Castle.DynamicProxy;
 using NUnit.Framework;
 using Autofac.Extras.DynamicProxy2;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace Autofac.Extras.Tests.DynamicProxy2
 {
@@ -14,57 +11,23 @@ namespace Autofac.Extras.Tests.DynamicProxy2
     public class AttributedInterfaceInterceptionFixture
     {
         [Intercept(typeof(AddOneInterceptor))]
-        [Intercept(typeof(AddTenInterceptor))]
-        public interface IHasIAndJ
+        public interface IHasI
         {
             int GetI();
-            int GetJ();
         }
 
-        public class C : IHasIAndJ
+        public class C : IHasI
         {
             public int I { get; private set; }
-            public int J { get; private set; }
 
             public C()
             {
-                I = J = 10;
+                I = 10;
             }
 
             public int GetI()
             {
                 return I;
-            }
-
-            public int GetJ()
-            {
-                return J;
-            }
-        }
-
-        class InterceptOnlyJ : IProxyGenerationHook
-        {
-            public void MethodsInspected()
-            {
-            }
-
-            public void NonProxyableMemberNotification(Type type, MemberInfo memberInfo)
-            {
-            }
-
-            public bool ShouldInterceptMethod(Type type, MethodInfo methodInfo)
-            {
-                return methodInfo.Name.Equals("GetJ");
-            }
-        }
-
-        class MyInterceptorSelector : IInterceptorSelector
-        {
-            public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
-            {
-                return method.Name == "GetI"
-                    ? interceptors.OfType<AddOneInterceptor>().ToArray<IInterceptor>()
-                    : interceptors.OfType<AddTenInterceptor>().ToArray<IInterceptor>();
             }
         }
 
@@ -73,18 +36,8 @@ namespace Autofac.Extras.Tests.DynamicProxy2
             public void Intercept(IInvocation invocation)
             {
                 invocation.Proceed();
-                if (invocation.Method.Name == "GetI" || invocation.Method.Name == "GetJ")
+                if (invocation.Method.Name == "GetI")
                     invocation.ReturnValue = 1 + (int)invocation.ReturnValue;
-            }
-        }
-
-        class AddTenInterceptor : IInterceptor
-        {
-            public void Intercept(IInvocation invocation)
-            {
-                invocation.Proceed();
-                if (invocation.Method.Name == "GetJ")
-                    invocation.ReturnValue = 10 + (int)invocation.ReturnValue;
             }
         }
 
@@ -103,10 +56,9 @@ namespace Autofac.Extras.Tests.DynamicProxy2
         public void FindsInterceptionAttributeOnReflectionComponent()
         {
             var builder = new ContainerBuilder();
-            builder.RegisterType<C>().As<IHasIAndJ>().EnableInterfaceInterceptors();
+            builder.RegisterType<C>().As<IHasI>().EnableInterfaceInterceptors();
             builder.RegisterType<AddOneInterceptor>();
-            builder.RegisterType<AddTenInterceptor>();
-            var cpt = builder.Build().Resolve<IHasIAndJ>();
+            var cpt = builder.Build().Resolve<IHasI>();
 
             Assert.AreEqual(11, cpt.GetI()); // proxied
         }
@@ -115,68 +67,11 @@ namespace Autofac.Extras.Tests.DynamicProxy2
         public void FindsInterceptionAttributeOnExpressionComponent()
         {
             var builder = new ContainerBuilder();
-            builder.Register(c => new C()).As<IHasIAndJ>().EnableInterfaceInterceptors();
+            builder.Register(c => new C()).As<IHasI>().EnableInterfaceInterceptors();
             builder.RegisterType<AddOneInterceptor>();
-            builder.RegisterType<AddTenInterceptor>();
-            var cpt = builder.Build().Resolve<IHasIAndJ>();
+            var cpt = builder.Build().Resolve<IHasI>();
 
             Assert.AreEqual(11, cpt.GetI()); // proxied
         }
-
-        [Test]
-        public void CanCreateMixinWithAttributeInterceptors()
-        {
-            var options = new ProxyGenerationOptions();
-            options.AddMixinInstance(new Dictionary<int, int>());
-
-            var builder = new ContainerBuilder();
-            builder.RegisterType<C>().As<IHasIAndJ>().EnableInterfaceInterceptors(options);
-            builder.RegisterType<AddOneInterceptor>();
-            builder.RegisterType<AddTenInterceptor>();
-            var cpt = builder.Build().Resolve<IHasIAndJ>();
-
-            var dict = cpt as IDictionary<int, int>;
-
-            Assert.IsNotNull(dict);
-
-            dict.Add(1, 2);
-
-            Assert.AreEqual(2, dict[1]);
-
-            dict.Clear();
-
-            Assert.IsEmpty(dict);
-        }
-
-        [Test]
-        public void CanInterceptOnlySpecificMethods()
-        {
-            var options = new ProxyGenerationOptions(new InterceptOnlyJ());
-
-            var builder = new ContainerBuilder();
-            builder.RegisterType<C>().As<IHasIAndJ>().EnableInterfaceInterceptors(options);
-            builder.RegisterType<AddOneInterceptor>();
-            builder.RegisterType<AddTenInterceptor>();
-            var cpt = builder.Build().Resolve<IHasIAndJ>();
-
-            Assert.AreEqual(10, cpt.GetI());
-            Assert.AreEqual(21, cpt.GetJ());
-        }
-
-        [Test]
-        public void CanInterceptMethodsWithSpecificInterceptors()
-        {
-            var options = new ProxyGenerationOptions { Selector = new MyInterceptorSelector() };
-
-            var builder = new ContainerBuilder();
-            builder.RegisterType<C>().As<IHasIAndJ>().EnableInterfaceInterceptors(options);
-            builder.RegisterType<AddOneInterceptor>();
-            builder.RegisterType<AddTenInterceptor>();
-            var cpt = builder.Build().Resolve<IHasIAndJ>();
-
-            Assert.AreEqual(11, cpt.GetI());
-            Assert.AreEqual(20, cpt.GetJ());
-        }
-
     }
 }
