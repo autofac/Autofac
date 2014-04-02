08451cea47c22ec1f8daa6844c650126f13734e3
alexmg <alex.meyergleaves@gmail.com>
2014-04-03 00:06:24 +1000

    Added AppSettingsModule to Autofac.Configuration based on Paul Stovell's "Convention-based Configuration for Autofac" blog post (http://paulstovell.com/blog/convention-configuration).

---
+++ Core/Source/Autofac.Configuration/AppSettingsModule.cs
@@ -0,0 +1,91 @@
+﻿// This software is part of the Autofac IoC container
+// Copyright © 2014 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Autofac.Configuration.Util;
+
+namespace Autofac.Configuration
+{
+    /// <summary>
+    /// Configures properties on other modules using settings from 
+    /// the appSettings section of the configuration file. These can
+    /// then be provided to component constructors as parameters.
+    /// </summary>
+    /// <remarks>
+    /// The convention for the appSettings key is "Module.PropertyName" (e.g. Email.Pop3Host).
+    /// Modules should be named with a "Module" suffix (e.g. EmailModule).
+    /// </remarks>
+    public class AppSettingsModule : Module
+    {
+        readonly IEnumerable<Module> _modules;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppSettingsModule"/> class.
+        /// </summary>
+        /// <param name="modules">The modules to configure using settings from 
+        /// the appSettings section of the configuration file.</param>
+        public AppSettingsModule(IEnumerable<Module> modules)
+        {
+            _modules = modules;
+        }
+
+        /// <summary>
+        /// Override to add registrations to the container.
+        /// </summary>
+        /// <param name="builder">The builder through which components can be registered.</param>
+        /// <remarks>
+        /// Note that the ContainerBuilder parameter is unique to this module.
+        /// </remarks>
+        protected override void Load(ContainerBuilder builder)
+        {
+            var settings = ConfigurationManager.AppSettings;
+            var keys = settings.AllKeys;
+
+            foreach (var setting in keys)
+            {
+                if (setting.Count(c => c == '.') != 1) continue;
+
+                var parts = setting.Split('.');
+                var moduleName = parts[0];
+                var propertyName = parts[1];
+                var value = settings[setting];
+
+                var module = _modules.FirstOrDefault(m => m.GetType().Name == moduleName + "Module");
+                if (module == null) continue;
+
+                var property = module.GetType().GetProperty(propertyName);
+                var convertedValue = TypeManipulation.ChangeToCompatibleType(value, property.PropertyType, property);
+
+                property.SetValue(module, convertedValue, null);
+            }
+
+            foreach (var module in _modules)
+                builder.RegisterModule(module);
+        }
+    }
+}
---
+++ Core/Tests/Autofac.Tests.Configuration/AppSettingsModuleFixture.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Autofac.Configuration;
+using NUnit.Framework;
+
+namespace Autofac.Tests.Configuration
+{
+    [TestFixture]
+    public class AppSettingsModuleFixture
+    {
+        [Test]
+        public void CanMapSimplePropertyFromAppSettings()
+        {
+            var module = new StringModule();
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ComponentWithStringParameter>();
+            builder.RegisterModule(new AppSettingsModule(new[] {module}));
+            var container = builder.Build();
+
+            var component = container.Resolve<ComponentWithStringParameter>();
+
+            Assert.That(component.StringParameter, Is.EqualTo("Autofac rocks!"));
+        }
+
+        [Test]
+        public void CanMapComplexPropertyFromAppSettings()
+        {
+            var module = new UriModule();
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ComponentWithUriParameter>();
+            builder.RegisterModule(new AppSettingsModule(new[] { module }));
+            var container = builder.Build();
+
+            var component = container.Resolve<ComponentWithUriParameter>();
+
+            Assert.That(component.UriParameter, Is.EqualTo(new Uri("http://autofac.org")));
+        }
+    }
+
+    public class StringModule : Module
+    {
+        public string StringSetting { get; set; }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ComponentWithStringParameter>().WithParameter("stringParameter", StringSetting);
+        }
+    }
+
+    public class UriModule : Module
+    {
+        public Uri UriSetting { get; set; }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ComponentWithUriParameter>().WithParameter("uriParameter", UriSetting);
+        }
+    }
+
+    public class ComponentWithStringParameter
+    {
+        public string StringParameter { get; private set; }
+
+        public ComponentWithStringParameter(string stringParameter)
+        {
+            StringParameter = stringParameter;
+        }
+    }
+
+    public class ComponentWithUriParameter
+    {
+        public Uri UriParameter { get; private set; }
+
+        public ComponentWithUriParameter(Uri uriParameter)
+        {
+            UriParameter = uriParameter;
+        }
+    }
+}
