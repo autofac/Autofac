fe1cc8d3a2f78daab0a51c3e1c8827e2f92ee453
alexmg <alex.meyergleaves@gmail.com>
2014-11-12 12:26:27 +0000

    Added sample ASP.NET vNext web application to test Autofac integration. This currently compiles alright but gets a MissingMethodException at runtime.

---
+++ Examples/AspNet50Example.WebApplication/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Security.Cookies;
+using Microsoft.Data.Entity;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using AspNet50Example.WebApplication.Models;
+using Autofac;
+using Autofac.Integration.AspNet;
+
+namespace AspNet50Example.WebApplication
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // Setup configuration sources
+            var configuration = new Configuration();
+            configuration.AddJsonFile("config.json");
+            configuration.AddEnvironmentVariables();
+
+            // Set up application services
+            app.UseServices(services =>
+            {
+                // Add EF services to the services container
+                services.AddEntityFramework()
+                    .AddSqlServer();
+
+                // Configure DbContext
+                services.SetupOptions<DbContextOptions>(options =>
+                {
+                    options.UseSqlServer(configuration.Get("Data:DefaultConnection:ConnectionString"));
+                });
+                
+                // Add Identity services to the services container
+                services.AddIdentitySqlServer<ApplicationDbContext, ApplicationUser>()
+                    .AddAuthentication();
+
+                // Add MVC services to the services container
+                services.AddMvc();
+
+                // Create the autofac container 
+                ContainerBuilder builder = new ContainerBuilder();
+
+                // Create the container and use the default application services as a fallback 
+                builder.Populate(
+                    services,
+                    fallbackServiceProvider: app.ApplicationServices);
+
+                // Build the container.
+                IContainer container = builder.Build();
+
+                // Resolve the service provider.
+                return container.Resolve<IServiceProvider>();
+            });
+
+            // Enable Browser Link support
+            app.UseBrowserLink();
+
+            // Add static files to the request pipeline
+            app.UseStaticFiles();
+
+            // Add cookie-based authentication to the request pipeline
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = ClaimsIdentityOptions.DefaultAuthenticationType,
+                LoginPath = new PathString("/Account/Login"),
+            });
+
+            // Add MVC to the request pipeline
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default", 
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+
+                routes.MapRoute(
+                    name: "api",
+                    template: "{controller}/{id?}");
+            });
+        }
+    }
+}
