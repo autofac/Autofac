3da0899a37e2bcb4f30073ad6ae6025a8e5e2ff7
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-01-29 00:19:21 +1000

    Removed SignalR integration which has been split out into a separate repository.

--- Core/Source/Autofac.Integration.SignalR/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Autofac.Integration.SignalR")]
-[assembly: ComVisible(false)]
-
-// SignalR is not marked CLS compliant.
-[assembly: CLSCompliant(false)]
