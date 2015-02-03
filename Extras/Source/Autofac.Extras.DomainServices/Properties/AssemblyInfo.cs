9a946b5d3b145a7b8ef0d847b3c7058bfd89b50b
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-03 22:50:41 +1000

    Removed Extras.DomainService after splitting out to separate repository.

--- Extras/Source/Autofac.Extras.DomainServices/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Autofac.Extras.DomainServices")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
