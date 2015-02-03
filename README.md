41f515df4390b3c44692413438d837c10dcaf6de
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-03 23:08:08 +1000

    Added solution and build configuration.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Autofac.Extras.DynamicProxy
 ===========================
 
 Interceptor and decorator support for Autofac IoC via Castle DynamicProxy
+
+[![Build status](https://ci.appveyor.com/api/projects/status/nx0urssttgc840eo?svg=true)](https://ci.appveyor.com/project/Autofac/autofac-extras-dynamicproxy)
--- src/Autofac.Extras.DynamicProxy2/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.DynamicProxy2/Properties/AssemblyInfo.cs
@@ -1,7 +1,21 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Autofac.Extras.DynamicProxy2")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
+
+[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
+[assembly: AssemblyProduct("Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("Interceptor and decorator support for Autofac IoC via Castle DynamicProxy")]
