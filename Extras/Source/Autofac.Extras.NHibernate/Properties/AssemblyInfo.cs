4681bfa6ee390e114156140a4fd5afc36da029b5
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-04 22:36:43 +1000

    Removed Extras.NHibernate after splitting to separate repository.

--- Extras/Source/Autofac.Extras.NHibernate/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Autofac.Extras.NHibernate")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
