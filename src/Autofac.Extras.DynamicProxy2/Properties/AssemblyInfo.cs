95f4243e8847358433b2602b61eb066cbcbe9239
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-04 21:08:22 +1000

    Fixed code analysis issues.

--- src/Autofac.Extras.DynamicProxy2/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.DynamicProxy2/Properties/AssemblyInfo.cs
@@ -2,10 +2,12 @@
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
+using System.Security;
 
 [assembly: AssemblyTitle("Autofac.Extras.DynamicProxy2")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
 
 [assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
 [assembly: AssemblyProduct("Autofac")]
