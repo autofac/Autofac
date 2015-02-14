540d97872d104d78cff36dfac565d07566d8d539
Kevin Cuzner <kevin@kevincuzner.com>
2015-02-14 12:55:04 -0700

    Added tests for open generics on the aggregate service

---
+++ Extras/Tests/Autofac.Extras.Tests.AggregateService/AggregateServiceGenericsFixture.cs
@@ -0,0 +1,42 @@
+﻿using Autofac.Extras.AggregateService;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Autofac.Extras.Tests.AggregateService
+{
+    [TestFixture]
+    public class AggregateServiceGenericsFixture
+    {
+        private IContainer _container;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterAggregateService<IOpenGenericAggregate>();
+            builder.RegisterGeneric(typeof(OpenGenericImpl<>))
+                .As(typeof(IOpenGeneric<>));
+
+            this._container = builder.Build();
+        }
+
+        /// <summary>
+        /// Attempts to resolve an open generic by a method call
+        /// </summary>
+        [Test]
+        public void Method_ResolveOpenGeneric()
+        {
+            var aggregateService = this._container.Resolve<IOpenGenericAggregate>();
+
+            var generic = aggregateService.GetOpenGeneric<object>();
+            Assert.That(generic, Is.Not.Null);
+
+            var ungeneric = aggregateService.GetResolvedGeneric();
+            Assert.That(ungeneric, Is.Not.Null);
+            Assert.That(ungeneric, Is.Not.SameAs(generic));
+        }
+    }
+}
---
+++ Extras/Tests/Autofac.Extras.Tests.AggregateService/OpenGenericImpl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Autofac.Extras.Tests.AggregateService
+{
+    public class OpenGenericImpl<T> : IOpenGeneric<T>
+    {
+    }
+}
