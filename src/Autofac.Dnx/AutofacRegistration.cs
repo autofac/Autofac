0c463737d34a1a2d3841a73b371bbc03eb9a2300
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-03-11 23:20:46 +1000

    Added unit tests for Autofac.Dnx.

--- src/Autofac.Dnx/AutofacRegistration.cs
+++ src/Autofac.Dnx/AutofacRegistration.cs
@@ -109,57 +109,5 @@ namespace Autofac.Dnx
 
             return registrationBuilder;
         }
-
-        private class AutofacServiceProvider : IServiceProvider
-        {
-            private readonly IComponentContext _componentContext;
-
-            public AutofacServiceProvider(IComponentContext componentContext)
-            {
-                _componentContext = componentContext;
-            }
-
-            public object GetService(Type serviceType)
-            {
-                return _componentContext.ResolveOptional(serviceType);
-            }
-        }
-
-        private class AutofacServiceScopeFactory : IServiceScopeFactory
-        {
-            private readonly ILifetimeScope _lifetimeScope;
-
-            public AutofacServiceScopeFactory(ILifetimeScope lifetimeScope)
-            {
-                _lifetimeScope = lifetimeScope;
-            }
-
-            public IServiceScope CreateScope()
-            {
-                return new AutofacServiceScope(_lifetimeScope.BeginLifetimeScope());
-            }
-        }
-
-        private class AutofacServiceScope : IServiceScope
-        {
-            private readonly ILifetimeScope _lifetimeScope;
-            private readonly IServiceProvider _serviceProvider;
-
-            public AutofacServiceScope(ILifetimeScope lifetimeScope)
-            {
-                _lifetimeScope = lifetimeScope;
-                _serviceProvider = _lifetimeScope.Resolve<IServiceProvider>();
-            }
-
-            public IServiceProvider ServiceProvider
-            {
-                get { return _serviceProvider; }
-            }
-
-            public void Dispose()
-            {
-                _lifetimeScope.Dispose();
-            }
-        }
     }
 }
