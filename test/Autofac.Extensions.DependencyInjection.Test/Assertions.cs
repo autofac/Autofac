4debce0dec215ff788cf9e9a86c0d59e27452eb9
Travis Illig <travis.illig@silverlining.ventures>
2016-05-11 14:58:48 -0700

    Finished StyleCop integration and style fixes.

--- test/Autofac.Extensions.DependencyInjection.Test/Assertions.cs
+++ test/Autofac.Extensions.DependencyInjection.Test/Assertions.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Autofac.Extensions.DependencyInjection.Test
 {
-    static class Assertions
+    internal static class Assertions
     {
         public static void AssertRegistered<TService>(this IComponentContext context)
         {
--- test/Autofac.Extensions.DependencyInjection.Test/SpecificationTests.cs
+++ test/Autofac.Extensions.DependencyInjection.Test/SpecificationTests.cs
@@ -4,16 +4,16 @@ using Microsoft.Extensions.DependencyInjection.Specification;
 
 namespace Autofac.Extensions.DependencyInjection.Test
 {
-	public class SpecificationTests : DependencyInjectionSpecificationTests
-	{
-		protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
-		{
-			var builder = new ContainerBuilder();
-	
-			builder.Populate(serviceCollection);
-	
-			IContainer container = builder.Build();
-			return container.Resolve<IServiceProvider>();
-		}
-	}	
+    public class SpecificationTests : DependencyInjectionSpecificationTests
+    {
+        protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.Populate(serviceCollection);
+
+            IContainer container = builder.Build();
+            return container.Resolve<IServiceProvider>();
+        }
+    }
 }
