8d13257212cf28e9a72d853376b4a5979ff35677
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2016-06-28 17:45:16 +1000

    Updates for .NET Core 1.0 and Preview 2 tooling.

--- build/Autofac.Build.psm1
+++ build/Autofac.Build.psm1
@@ -43,7 +43,7 @@ function Install-DotNetCli
   # Download the dotnet CLI install script
   if (!(Test-Path .\dotnet\install.ps1))
   {
-    Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1" -OutFile ".\.dotnet\dotnet-install.ps1"
+    Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.ps1" -OutFile ".\.dotnet\dotnet-install.ps1"
   }
 
   # Run the dotnet CLI install
