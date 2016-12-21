36e266f560d51a4bd2d7c00913d8ece91b41f255
Michał Sitko <mic.sitko@gmail.com>
2016-12-21 22:35:52 +0100

    resolve generics constrained with generic parameters

--- test/Autofac.Test/Features/OpenGenerics/ComplexGenericsTests.cs
+++ test/Autofac.Test/Features/OpenGenerics/ComplexGenericsTests.cs
@@ -220,8 +220,25 @@ namespace Autofac.Test.Features.OpenGenerics
             builder.RegisterGeneric(typeof(MultiConstrained<,>));
 
             var container = builder.Build();
-
+            
             Assert.True(container.IsRegistered<MultiConstrained<int, IConstrainedConstraintWithOnlyAddedArgument<string>>>());
+        }
+
+        public class ConstrainedWithGenericParameter<T1, T2>
+            where T1 : T2
+        {
+        }
+
+        [Fact]
+        public void CanResolveComponentWhenGenericParameterIsConstrainedWithOtherGenericParameter()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterGeneric(typeof(ConstrainedWithGenericParameter<,>));
+
+            var container = builder.Build();
+
+            Assert.True(container.IsRegistered<ConstrainedWithGenericParameter<int, object>>());
         }
 
         [Fact(Skip = "Issue #688")]
