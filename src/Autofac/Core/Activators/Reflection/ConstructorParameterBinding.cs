bf4a643b9ab783667450e55c0317a0a986afc45a
Tzach Shabtay <tzachshabtay@gmail.com>
2017-03-31 21:45:08 -0400

    Remove trailing whitespaces

--- src/Autofac/Core/Activators/Reflection/ConstructorParameterBinding.cs
+++ src/Autofac/Core/Activators/Reflection/ConstructorParameterBinding.cs
@@ -62,9 +62,9 @@ namespace Autofac.Core.Activators.Reflection
         {
             // On IOS, when the mono linker is enabled (and it's enabled by default) it strips out all parts of
             // the framework which are not in use. The linker does not support reflection, however, therefore it
-            // does not recognize the call to 'GetRuntimeMethod(nameof(Convert.ChangeType)' (which is called from 
-            // a method in this class) as using the 'Convert.ChangeType' method and strips it away unless it's  
-            // explicitly used somewhere else, crashing Autofac if that method is called. 
+            // does not recognize the call to 'GetRuntimeMethod(nameof(Convert.ChangeType)' (which is called from
+            // a method in this class) as using the 'Convert.ChangeType' method and strips it away unless it's
+            // explicitly used somewhere else, crashing Autofac if that method is called.
             // Therefore, by explicitly calling it here, we're playing nicely with the linker and making sure
             // Autofac works on IOS.
             Convert.ChangeType((object)1234f, typeof(float));
