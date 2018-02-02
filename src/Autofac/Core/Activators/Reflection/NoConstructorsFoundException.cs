d22c8d52c1257f17ebe1737329b97a2bc8ee49f1
Travis Illig <travis.illig@fiserv.com>
2018-02-02 09:04:52 -0800

    Minor doc/naming updates.

--- src/Autofac/Core/Activators/Reflection/NoConstructorsFoundException.cs
+++ src/Autofac/Core/Activators/Reflection/NoConstructorsFoundException.cs
@@ -33,52 +33,61 @@ namespace Autofac.Core.Activators.Reflection
         /// <summary>
         /// Initializes a new instance of the <see cref="NoConstructorsFoundException"/> class.
         /// </summary>
-        /// <param name="type">The type whose constructor was not found.</param>
-        public NoConstructorsFoundException(Type type)
-            : this(type, FormatMessage(type))
+        /// <param name="offendingType">The <see cref="System.Type"/> whose constructor was not found.</param>
+        public NoConstructorsFoundException(Type offendingType)
+            : this(offendingType, FormatMessage(offendingType))
         {
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NoConstructorsFoundException"/> class.
         /// </summary>
-        /// <param name="type">The type whose constructor was not found.</param>
+        /// <param name="offendingType">The <see cref="System.Type"/> whose constructor was not found.</param>
         /// <param name="message">Exception message.</param>
-        public NoConstructorsFoundException(Type type, string message)
+        public NoConstructorsFoundException(Type offendingType, string message)
             : base(message)
         {
-            this.OffendingType = type;
+            if (offendingType == null) throw new ArgumentNullException(nameof(offendingType));
+            this.OffendingType = offendingType;
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NoConstructorsFoundException"/> class.
         /// </summary>
-        /// <param name="type">The type whose constructor was not found.</param>
+        /// <param name="offendingType">The <see cref="System.Type"/> whose constructor was not found.</param>
         /// <param name="innerException">The inner exception.</param>
-        public NoConstructorsFoundException(Type type, Exception innerException)
-            : this(type, FormatMessage(type), innerException)
+        public NoConstructorsFoundException(Type offendingType, Exception innerException)
+            : this(offendingType, FormatMessage(offendingType), innerException)
         {
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NoConstructorsFoundException"/> class.
         /// </summary>
-        /// <param name="type">The type whose constructor was not found.</param>
+        /// <param name="offendingType">The <see cref="System.Type"/> whose constructor was not found.</param>
         /// <param name="message">Exception message.</param>
         /// <param name="innerException">The inner exception.</param>
-        public NoConstructorsFoundException(Type type, string message, Exception innerException)
+        public NoConstructorsFoundException(Type offendingType, string message, Exception innerException)
             : base(message, innerException)
         {
-            this.OffendingType = type;
+            if (offendingType == null) throw new ArgumentNullException(nameof(offendingType));
+            this.OffendingType = offendingType;
         }
 
+        /// <summary>
+        /// Gets the type without found constructors.
+        /// </summary>
+        /// <value>
+        /// A <see cref="System.Type"/> that was processed by an <see cref="IConstructorFinder"/>
+        /// or similar mechanism and was determined to have no available constructors.
+        /// </value>
         public Type OffendingType { get; private set; }
 
-        private static string FormatMessage(Type type)
+        private static string FormatMessage(Type offendingType)
         {
-            if (type == null) throw new ArgumentNullException(nameof(type));
+            if (offendingType == null) throw new ArgumentNullException(nameof(offendingType));
 
-            return string.Format(CultureInfo.CurrentCulture, NoConstructorsFoundExceptionResources.Message, type.FullName);
+            return string.Format(CultureInfo.CurrentCulture, NoConstructorsFoundExceptionResources.Message, offendingType.FullName);
         }
     }
 }
