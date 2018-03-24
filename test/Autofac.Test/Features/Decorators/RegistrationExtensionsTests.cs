4c68877a19142db75577ca4c6d0806339fc94bad
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2018-03-24 17:39:07 +1000

    Added unit tests for argument checks on new decorator registration methods.

---
+++ test/Autofac.Test/Features/Decorators/RegistrationExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac.Core;
+using Xunit;
+
+namespace Autofac.Test.Features.Decorators
+{
+    public class RegistrationExtensionsTests
+    {
+        [Fact]
+        public void RegisterDecoratorRequiresDecoratorType()
+        {
+            var builder = new ContainerBuilder();
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => builder.RegisterDecorator(null, typeof(object)));
+
+            Assert.Equal("decoratorType", exception.ParamName);
+        }
+
+        [Fact]
+        public void RegisterDecoratorRequiresServiceType()
+        {
+            var builder = new ContainerBuilder();
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => builder.RegisterDecorator(typeof(object), null));
+
+            Assert.Equal("serviceType", exception.ParamName);
+        }
+
+        [Fact]
+        public void RegisterDecoratorRequiresDecoratorFunc()
+        {
+            var builder = new ContainerBuilder();
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => builder.RegisterDecorator(
+                    default(Func<IComponentContext, IEnumerable<Parameter>, object, object>)));
+
+            Assert.Equal("decorator", exception.ParamName);
+        }
+
+        [Fact]
+        public void RegisterGenericDecoratorRequiresDecoratorType()
+        {
+            var builder = new ContainerBuilder();
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => builder.RegisterGenericDecorator(null, typeof(object)));
+
+            Assert.Equal("decoratorType", exception.ParamName);
+        }
+
+        [Fact]
+        public void RegisterGenericDecoratorRequiresServiceType()
+        {
+            var builder = new ContainerBuilder();
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => builder.RegisterGenericDecorator(typeof(object), null));
+
+            Assert.Equal("serviceType", exception.ParamName);
+        }
+    }
+}
