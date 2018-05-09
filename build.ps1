493b02022dfb2960eddac7592c76c6e054e02ba8
Travis Illig <travis.illig@fiserv.com>
2018-05-09 11:01:05 -0700

    Fixed benchmark artifact copy.

--- build.ps1
+++ build.ps1
@@ -48,7 +48,7 @@ Get-DotNetProjectDirectory -RootPath $PSScriptRoot\test | Where-Object { $_ -ino
 # Benchmark
 if ($Bench) {
 	Get-DotNetProjectDirectory -RootPath $PSScriptRoot\bench | Invoke-Test
-	mv "$PSScriptRoot\BenchmarkDotNet.Artifacts" "$PSScriptRoot\artifacts\benchmarks"
+  Get-ChildItem -Path $PSScriptRoot\bench -Filter "BenchmarkDotNet.Artifacts" -Directory -Recurse | Move-Item -Destination "$PSScriptRoot\artifacts\benchmarks"
 }
 
 # Finished
