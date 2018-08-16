1738b769f617bfa0ce2bda1de14d01df2bd9f4b5
Travis Illig <travis.illig@fiserv.com>
2018-08-16 08:30:14 -0700

    Memory optimization from #919: Autofac.Features.OpenGenerics.OpenGenericDecoratorRegistrationSource.

--- src/Autofac/Features/OpenGenerics/OpenGenericDecoratorRegistrationSource.cs
+++ src/Autofac/Features/OpenGenerics/OpenGenericDecoratorRegistrationSource.cs
@@ -77,13 +77,20 @@ namespace Autofac.Features.OpenGenerics
             return Enumerable.Empty<IComponentRegistration>();
         }
 
-        private static IEnumerable<Parameter> AddDecoratedComponentParameter(Type decoratedParameterType, IComponentRegistration decoratedComponent, IEnumerable<Parameter> configuredParameters)
+        private static Parameter[] AddDecoratedComponentParameter(Type decoratedParameterType, IComponentRegistration decoratedComponent, IList<Parameter> configuredParameters)
         {
             var parameter = new ResolvedParameter(
                 (pi, c) => pi.ParameterType == decoratedParameterType,
                 (pi, c) => c.ResolveComponent(decoratedComponent, Enumerable.Empty<Parameter>()));
 
-            return new[] { parameter }.Concat(configuredParameters);
+            var resultArray = new Parameter[configuredParameters.Count + 1];
+            resultArray[0] = parameter;
+            for (int i = 0; i < configuredParameters.Count; i++)
+            {
+                resultArray[i + 1] = configuredParameters[i];
+            }
+
+            return resultArray;
         }
 
         public bool IsAdapterForIndividualComponents => true;
