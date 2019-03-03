7ff61cc554e0f3fdbb67bf807b7e4c31e4a9fa21
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2019-03-03 19:52:59 +1000

    Drop explanatory comment

--- test/Autofac.Test/Core/DependencyResolutionExceptionTests.cs
+++ test/Autofac.Test/Core/DependencyResolutionExceptionTests.cs
@@ -45,22 +45,6 @@ namespace Autofac.Test.Core
                 ex = Assert.Throws<DependencyResolutionException>(() => container.Resolve<C>());
             }
 
-            // Without unwrapping, the exception message is:
-            //
-            // An error occurred during the activation of a particular registration. See the inner exception
-            // for details. Registration: Activator = C (ReflectionActivator), Services =
-            // [Autofac.Test.ExceptionReportingTests+C], Lifetime = Autofac.Core.Lifetime.CurrentScopeLifetime,
-            // Sharing = None, Ownership = OwnedByLifetimeScope ---> An error occurred during the activation of
-            // a particular registration. See the inner exception for details. Registration: Activator = B
-            // (DelegateActivator), Services = [Autofac.Test.ExceptionReportingTests+B], Lifetime =
-            // Autofac.Core.Lifetime.CurrentScopeLifetime, Sharing = None, Ownership = OwnedByLifetimeScope --->
-            // An error occurred during the activation of a particular registration. See the inner exception for
-            // details. Registration: Activator = A (ReflectionActivator), Services =
-            // [Autofac.Test.ExceptionReportingTests+A], Lifetime = Autofac.Core.Lifetime.CurrentScopeLifetime,
-            // Sharing = None, Ownership = OwnedByLifetimeScope ---> An exception was thrown while invoking the
-            // constructor 'Void .ctor()' on type 'A'. ---> This is the original exception. (See inner exception
-            // for details.) (See inner exception for details.) (See inner exception for details.) (See inner
-            // exception for details.)
             var n = GetType().FullName;
             Assert.Equal($"An exception was thrown while activating {n}+C -> λ:{n}+B -> {n}+A.", ex.Message);
 
