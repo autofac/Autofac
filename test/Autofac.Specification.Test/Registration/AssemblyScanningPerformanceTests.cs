d64edac44a0344867cd4e526b60f5864d8c85909
alexmg <alex.meyergleaves@gmail.com>
2019-08-15 23:00:29 +1000

    Fix build warnings

--- test/Autofac.Specification.Test/Registration/AssemblyScanningPerformanceTests.cs
+++ test/Autofac.Specification.Test/Registration/AssemblyScanningPerformanceTests.cs
@@ -1,17 +1,15 @@
+using System.Diagnostics;
+using System.Globalization;
+using Xunit;
+using Xunit.Abstractions;
+
 namespace Autofac.Specification.Test.Registration
 {
-    using System.Diagnostics;
-    using Xunit;
-    using Xunit.Abstractions;
-
     public class AssemblyScanningPerformanceTests
     {
-        private readonly ITestOutputHelper output;
+        private readonly ITestOutputHelper _output;
 
-        public AssemblyScanningPerformanceTests(ITestOutputHelper output)
-        {
-            this.output = output;
-        }
+        public AssemblyScanningPerformanceTests(ITestOutputHelper output) => _output = output;
 
         [Fact]
         public void MeasurePerformance()
@@ -19,15 +17,15 @@ namespace Autofac.Specification.Test.Registration
             var builder = new ContainerBuilder();
             for (var i = 0; i < 1000; i++)
             {
-                //just to simulate a lot of "scanning" with few (in this case zero) "hits"
+                // Just to simulate a lot of "scanning" with few (in this case zero) "hits"
                 builder.RegisterAssemblyTypes(GetType().Assembly).Where(x => false);
             }
 
             var stopwatch = Stopwatch.StartNew();
             builder.Build().Dispose();
 
-            //after fix drops from ~500ms to ~100ms
-            output.WriteLine(stopwatch.Elapsed.TotalMilliseconds.ToString());
+            // After fix drops from ~500ms to ~100ms
+            _output.WriteLine(stopwatch.Elapsed.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
         }
     }
 }
