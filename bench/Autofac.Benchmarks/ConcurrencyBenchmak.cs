81e22bde6ba3a2e0d780835632ac065a2ac30914
alistairjevans <alistairjevans@gmail.com>
2019-12-21 11:26:39 +0000

    Correct defect with concurrency benchmarks and adding a nested scope concurrency benchmark.

--- bench/Autofac.Benchmarks/ConcurrencyBenchmak.cs
+++ bench/Autofac.Benchmarks/ConcurrencyBenchmak.cs
@@ -26,10 +26,10 @@ namespace Autofac.Benchmarks
             _container = builder.Build();
         }
 
-        [Params(10 /*, 100, 1_000 */)]
+        [Params(100 /*, 100, 1_000 */)]
         public int ResolveTaskCount { get; set; }
 
-        [Params(10 /*, 1_000, 10_000 */)]
+        [Params(100 /*, 1_000, 10_000 */)]
         public int ResolvesPerTask { get; set; }
 
         [Benchmark]
@@ -57,7 +57,7 @@ namespace Autofac.Benchmarks
                 tasks.Add(task);
             }
 
-            Task.WhenAll(tasks);
+            Task.WhenAll(tasks).Wait();
         }
 
         internal class A
