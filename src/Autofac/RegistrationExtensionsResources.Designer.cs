74ef53f02030a4f4aab7334b0b25a568c311c67b
Jan Zahradník <jan.zahradnik@gmail.com>
2020-05-17 21:57:53 +0200

    WithProperty by expression

--- src/Autofac/RegistrationExtensionsResources.Designer.cs
+++ src/Autofac/RegistrationExtensionsResources.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace Autofac {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -20,7 +19,7 @@ namespace Autofac {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class RegistrationExtensionsResources {
@@ -40,7 +39,7 @@ namespace Autofac {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Autofac.RegistrationExtensionsResources", typeof(RegistrationExtensionsResources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Autofac.RegistrationExtensionsResources", typeof(RegistrationExtensionsResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -67,6 +66,15 @@ namespace Autofac {
         internal static string DecoratorRequiresInstanceParameter {
             get {
                 return ResourceManager.GetString("DecoratorRequiresInstanceParameter", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Expression does not refer to a property.
+        /// </summary>
+        internal static string ExpressionDoesNotReferToProperty {
+            get {
+                return ResourceManager.GetString("ExpressionDoesNotReferToProperty", resourceCulture);
             }
         }
         
