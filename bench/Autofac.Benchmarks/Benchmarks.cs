fe5b4e2e7aedafac937beef69b11eb1d05dd2aa3
Alistair Evans <alistairjevans@gmail.com>
2020-06-03 13:14:13 +0100

    Added a benchmark profiling CLI to make it easier to profile our benchmarks.

--- bench/Autofac.Benchmarks/Benchmarks.cs
+++ bench/Autofac.Benchmarks/Benchmarks.cs
@@ -4,9 +4,9 @@ using Autofac.Benchmarks.Decorators;
 
 namespace Autofac.Benchmarks
 {
-    internal static class Benchmarks
+    public static class Benchmarks
     {
-        internal static readonly Type[] All =
+        public static readonly Type[] All =
         {
             typeof(ChildScopeResolveBenchmark),
             typeof(ConcurrencyBenchmark),
