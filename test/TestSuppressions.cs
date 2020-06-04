18de2d6a1951a8803a4e81bb93aef20ad79ef322
Alistair Evans <alistairjevans@gmail.com>
2020-06-04 16:57:09 +0100

    Use Directory.Build.props instead of suppression file.

--- test/TestSuppressions.cs
+++
@@ -1,5 +0,0 @@
-﻿// Suppressions file for test projects.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Test projects don't need docs.")]
