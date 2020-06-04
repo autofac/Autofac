bfda66015d009df9e82152ed3877ff481ed57932
Alistair Evans <alistairjevans@gmail.com>
2020-06-04 12:40:07 +0100

    Suppression file rename.

---
+++ test/TestSuppressions.cs
@@ -0,0 +1,5 @@
+﻿// Suppressions file for test projects.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Test projects don't need docs.")]
