9777ad6762449778434527d6f751e23a0e089c87
Travis Illig <tillig@paraesthesia.com>
2020-07-17 10:42:26 -0700

    Namespace move, forgot the resources location.

--- src/Autofac/Diagnostics/TracerMessages.Designer.cs
+++ src/Autofac/Diagnostics/TracerMessages.Designer.cs
@@ -39,7 +39,7 @@ namespace Autofac.Diagnostics {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Autofac.Core.Diagnostics.TracerMessages", typeof(TracerMessages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Autofac.Diagnostics.TracerMessages", typeof(TracerMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
