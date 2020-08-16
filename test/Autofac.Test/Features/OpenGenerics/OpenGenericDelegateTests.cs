031c581be09285582e81b7ff4798d73f549c0781
Alistair Evans <alistairjevans@gmail.com>
2020-08-16 13:59:30 +0100

    Add negative test for the registration source.

--- test/Autofac.Test/Features/OpenGenerics/OpenGenericDelegateTests.cs
+++ test/Autofac.Test/Features/OpenGenerics/OpenGenericDelegateTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Autofac.Core;
+using Autofac.Core.Registration;
 using Xunit;
 
 namespace Autofac.Test.Features.OpenGenerics
@@ -8,6 +10,10 @@ namespace Autofac.Test.Features.OpenGenerics
     public class OpenGenericDelegateTests
     {
         private interface IInterfaceA<T>
+        {
+        }
+
+        private interface IInterfaceB<T>
         {
         }
 
@@ -30,6 +36,19 @@ namespace Autofac.Test.Features.OpenGenerics
             var implementedType = instance.GetType().GetGenericTypeDefinition();
 
             Assert.Equal(typeof(ImplementationA<>), implementedType);
+        }
+
+        [Fact]
+        public void DoesNotResolveForDifferentGenericService()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterGeneric((ctxt, types) => Activator.CreateInstance(typeof(ImplementationA<>).MakeGenericType(types)))
+                   .As(typeof(IInterfaceA<>));
+
+            var container = builder.Build();
+
+            Assert.Throws<ComponentNotRegisteredException>(() => container.Resolve<IInterfaceB<string>>());
         }
     }
 }
