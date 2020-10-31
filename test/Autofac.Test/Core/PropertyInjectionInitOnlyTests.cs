75ebc80ab120b43568aaae54d8e39d33f207e1a2
Alistair Evans <alistairjevans@gmail.com>
2020-10-31 13:12:40 +0000

    Add test for init-only properties.

---
+++ test/Autofac.Test/Core/PropertyInjectionInitOnlyTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+#if NET5_0
+
+namespace Autofac.Test.Core
+{
+    public class PropertyInjectionInitOnlyTests
+    {
+        public class HasInitOnlyProperties
+        {
+            public string InjectedString { get; init; }
+        }
+
+        [Fact]
+        public void CanInjectInitOnlyProperties()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<HasInitOnlyProperties>().PropertiesAutowired();
+            builder.Register(ctxt => "hello world");
+            var container = builder.Build();
+
+            var instance = container.Resolve<HasInitOnlyProperties>();
+
+            Assert.Equal("hello world", instance.InjectedString);
+        }
+    }
+}
+
+#endif
