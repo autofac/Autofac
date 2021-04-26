61b9463852b8d6fed4ec76510d62134331e4bdc8
Alistair Evans <alistairjevans@gmail.com>
2021-04-26 08:27:30 +0100

    Resolve IEnumerable reference

--- src/Autofac/ILifetimeScope.cs
+++ src/Autofac/ILifetimeScope.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using Autofac.Builder;
 using Autofac.Core;
 using Autofac.Core.Lifetime;
