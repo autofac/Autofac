4cb24f167446d7edaf318a968c1d7230289de195
Travis Illig <tillig@paraesthesia.com>
2023-02-08 09:48:25 -0800

    Add file header.

--- test/Autofac.Test.Scenarios.LoadContext/Service1.cs
+++ test/Autofac.Test.Scenarios.LoadContext/Service1.cs
@@ -1,4 +1,7 @@
-﻿namespace A;
+﻿// Copyright (c) Autofac Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace A;
 
 public class Service1
 {
