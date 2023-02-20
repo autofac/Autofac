f3f4bef676f1ff4d943086b604c8ffa2b6b30b8b
Alistair Evans <alistairjevans@gmail.com>
2023-02-20 17:59:31 +0000

    Fix scenario project target frameworks.

--- test/Autofac.Test/Util/Cache/TypeAssemblyReferenceProviderTests.cs
+++ test/Autofac.Test/Util/Cache/TypeAssemblyReferenceProviderTests.cs
@@ -1,4 +1,7 @@
-﻿using Autofac.Builder;
+﻿// Copyright (c) Autofac Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Autofac.Builder;
 using Autofac.Core;
 using Autofac.Features.Indexed;
 using Autofac.Util.Cache;
