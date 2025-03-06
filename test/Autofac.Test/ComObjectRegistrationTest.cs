299fe3a976f93639f69a26d07398071bab25df97
Flepp Jann <jflepp@hamilton.ch>
2025-03-06 16:25:15 +0100

    Add ComObjectRegistrationTest.cs

---
+++ test/Autofac.Test/ComObjectRegistrationTest.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Autofac Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Autofac.Test;
+
+public class ComObjectRegistrationTest
+{
+    private const string WindowsScriptingFileSystemObjectProgramId = "Scripting.FileSystemObject";
+
+    [Fact]
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "File is excluded from non-Windows platforms.")]
+    public void AsTest()
+    {
+        var fsoType = Type.GetTypeFromProgID(WindowsScriptingFileSystemObjectProgramId);
+        var fso = Activator.CreateInstance(fsoType);
+
+        var builder = new ContainerBuilder();
+        builder.RegisterInstance(fso).As<string>();
+        _ = builder.Build();
+    }
+}
