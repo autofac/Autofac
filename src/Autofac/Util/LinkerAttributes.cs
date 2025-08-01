9bd2239b78e499da1b2b4002fa7fceaf2c6f55e3
Christian Prochnow <christian.prochnow77@gmail.com>
2025-08-01 16:57:36 +0200

    Make 'RequiresUnreferencedCodeAttribute' internal.

--- src/Autofac/Util/LinkerAttributes.cs
+++ src/Autofac/Util/LinkerAttributes.cs
@@ -113,7 +113,7 @@ internal sealed class DynamicallyAccessedMembersAttribute : Attribute
 /// code from an application.
 /// </remarks>
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor, Inherited = false)]
-public sealed class RequiresUnreferencedCodeAttribute : Attribute
+internal sealed class RequiresUnreferencedCodeAttribute : Attribute
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="RequiresUnreferencedCodeAttribute"/> class
