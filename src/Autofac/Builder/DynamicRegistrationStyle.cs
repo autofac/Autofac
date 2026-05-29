4c363940143deb947396c3886779e66b6a42553d
Travis Illig <tillig@paraesthesia.com>
2026-05-28 17:43:44 -0700

    Suppress public API change.

--- src/Autofac/Builder/DynamicRegistrationStyle.cs
+++ src/Autofac/Builder/DynamicRegistrationStyle.cs
@@ -6,6 +6,7 @@ namespace Autofac.Builder;
 /// <summary>
 /// Registration style for dynamic registrations.
 /// </summary>
+[SuppressMessage("S2094", "S2094", Justification = "Registration style may not have multiple inheritance, so a class is used instead of an interface. Changing this would be a breaking API change.")]
 public class DynamicRegistrationStyle
 {
 }
